03dc32e54ef7eb85d71bc084662d0f955f17752c
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-07-07 13:19:45 +0530

    feat: add heading and coming soon text

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<section
+	class="prose grid grid-cols-1 items-center content-center max-w-5xl mx-auto"
+>
+	<div class="py-20">
+		<h1 class="text-center text-6xl text-gray-400">
+			Create presentations in seconds!
+		</h1>
+
+		<div class="flex justify-center">
+			<button class="btn btn-lg btn-disabled">(Coming Soon)</button>
+		</div>
+	</div>
+</section>
