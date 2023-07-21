b44562e01bcfe5f748cda52bd09410dbb4a89a30
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-07-21 22:44:26 +0530

    fix: update path of navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import '../app.css';
-	import Navbar from '$lib/Navbar.svelte';
+	import Navbar from '$lib/ui/Navbar.svelte';
 </script>
 
 <Navbar />
 
-<main class="py-2 max-w-7xl mx-auto">
+<main class="py-6 px-8 mx-auto">
 	<slot />
 </main>
