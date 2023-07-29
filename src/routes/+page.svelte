22a953ca37195317fd47ac302392fbab5958c4df
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-07-29 11:11:16 +0530

    feat: update home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,24 @@
 <script lang="ts">
+	export const prerender = true;
 </script>
 
-<section
-	class="prose grid grid-cols-1 items-center content-center max-w-5xl mx-auto"
->
-	<div class="py-20">
-		<h1 class="text-center text-6xl text-gray-400">
-			Create presentations in seconds!
-		</h1>
+<section class="text-center py-8">
+	<hgroup>
+		<h1>Create Presentation In Seconds!</h1>
+		<p>
+			Riju is the super-fast way to create presentations without the hassle of
+			drag-and-drop, alignment, etc.
+		</p>
+		<a href="/" role="button">Sign up (Waiting List)</a>
+		<a class="secondar outline" href="/dashboard" role="button">Dashboard</a>
+	</hgroup>
 
-		<div class="flex justify-center">
-			<button class="btn btn-lg btn-disabled">(Coming Soon)</button>
-		</div>
-	</div>
+	<iframe
+		class="w-full max-w-2xl h-80 mx-auto mt-8"
+		src="https://www.youtube-nocookie.com/embed/H5Gj1W32Qao"
+		title="YouTube video player"
+		frameborder="0"
+		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+		allowfullscreen
+	/>
 </section>
