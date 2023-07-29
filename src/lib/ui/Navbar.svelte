e0987b3090516263d851fc352b440bd770864303
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-07-29 11:10:18 +0530

    chore: update navbar

--- src/lib/ui/Navbar.svelte
+++ src/lib/ui/Navbar.svelte
@@ -1,3 +1,15 @@
-<div class="navbar bg-base-100">
-	<a class="btn btn-ghost normal-case text-2xl" href="/">Riju</a>
-</div>
+<script lang="ts">
+</script>
+
+<nav class="container-fluid">
+	<ul>
+		<li><a href="/">Riju</a></li>
+	</ul>
+	<ul>
+		<li><a href="/docs">Docs</a></li>
+		<li><a href="/contact-us">Contact Us</a></li>
+		<li>
+			<a href="/dashboard/login" role="button">Login</a>
+		</li>
+	</ul>
+</nav>
