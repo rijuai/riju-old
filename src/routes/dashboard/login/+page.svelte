25cac195d8a2c2784b70deab4dd0ed6e92cf1fdd
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-07-29 11:22:30 +0530

    feat: create login page

---
+++ src/routes/dashboard/login/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<article class="card max-w-md mx-auto">
+	<form>
+		<h3>Signup / Login</h3>
+		<input type="text" placeholder="Email" />
+		<button>Send Magic Link</button>
+		<p>
+			Facing problems with Riju? <a href="/contact-us">contact us</a>
+		</p>
+	</form>
+</article>
