fe8e60f091a8e6f91d65c8284b7200e589342dfe
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-07-30 20:14:56 +0530

    feat: create dashboard navbar

---
+++ src/lib/ui/dashboard/Navbar.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { PenBox, Play } from 'lucide-svelte';
+</script>
+
+<nav class="container-fluid">
+	<ul>
+		<li><a href="/">Riju</a></li>
+	</ul>
+	<ul>
+		<li>
+			<a class="secondary outline" href="/">Change Theme</a>
+		</li>
+		<li>
+			<a class="secondary" href="/dashboard/present">
+				<Play size="20" />
+				Present</a
+			>
+		</li>
+		<li>
+			<a class="primary" href="/dashboard/presentation" role="button">
+				<PenBox size="20" />
+				Create</a
+			>
+		</li>
+	</ul>
+</nav>
