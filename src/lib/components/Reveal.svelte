66bf4fbda330c73c3efb8068d3cacb5ecc4cd63e
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-07-30 20:57:42 +0530

    feat: create present page

---
+++ src/lib/components/Reveal.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import Reveal from 'reveal.js';
+	import 'reveal.js/dist/reveal.css';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		let deck = new Reveal({
+			controls: true,
+			progress: true,
+			history: true,
+			center: true,
+			transition: 'slide',
+			embedded: true,
+			autoAnimate: true,
+		});
+
+		deck.initialize();
+	});
+</script>
+
+<section
+	class="presentation reveal aspect-video bg-gradient-to-tr from-blue-300 to-blue-600"
+>
+	<div class="slides">
+		<section>
+			<h2>Riju</h2>
+			<p>A PowerPoint alternative.</p>
+		</section>
+		<section>
+			<h2 class="text-left">Why use Riju?</h2>
+			<ol class="text-left list-decimal">
+				<li>Easy to use</li>
+				<li>Super fast to build</li>
+				<li>Super affordable</li>
+				<li>Very polished product</li>
+			</ol>
+		</section>
+	</div>
+</section>
+
+<style>
+</style>
---
+++ src/routes/dashboard/present/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Reveal from '$lib/components/Reveal.svelte';
+</script>
+
+<Reveal />
