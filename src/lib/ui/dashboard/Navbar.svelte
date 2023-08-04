28caa8d9d63c4b5713b922d1fb06d2823c8d30f6
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-08-04 16:54:29 +0530

    feat: update navbar

--- src/lib/ui/dashboard/Navbar.svelte
+++ src/lib/ui/dashboard/Navbar.svelte
@@ -1,20 +1,24 @@
 <script lang="ts">
-	import { PenBox, Play } from 'lucide-svelte';
+	import { LayoutDashboard, PenBox, Play } from 'lucide-svelte';
 </script>
 
 <nav class="container-fluid">
 	<ul>
 		<li><a href="/">Riju</a></li>
+		<li>
+			<a class="secondary" href="/dashboard">
+				<LayoutDashboard size="20" />
+			</a>
+		</li>
 	</ul>
 	<ul>
 		<li>
 			<a class="secondary outline" href="/">Change Theme</a>
 		</li>
 		<li>
-			<a class="secondary" href="/dashboard/present">
+			<a class="secondary" href="/present">
 				<Play size="20" />
-				Present</a
-			>
+			</a>
 		</li>
 		<li>
 			<a class="primary" href="/dashboard/presentation" role="button">
