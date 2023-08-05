616536273175559c0677b7e3dc03eae1c1ae1509
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-08-05 22:54:26 +0530

    feat: update login page ui

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import Navbar from '$lib/ui/Navbar.svelte';
+
+	let email: String, password: String;
+</script>
+
+<svelte:head>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/@picocss/pico@2.0.0-alpha1/css/pico.min.css"
+	/>
+</svelte:head>
+
+<Navbar />
+
+<main>
+	<article class="card max-w-sm mx-auto">
+		<header>
+			<h1 class="text">Sign up / Login</h1>
+		</header>
+		<form>
+			<fieldset>
+				<label>
+					Email
+					<input
+						type="email"
+						name="email"
+						placeholder="Email"
+						autocomplete="email"
+					/>
+				</label>
+			</fieldset>
+
+			<input type="submit" value="Send Magic Link" />
+		</form>
+		<footer>
+			<p class="mt-2">
+				Facing problems with Riju? <a href="/contact-us">Contact Us</a>
+			</p>
+		</footer>
+	</article>
+</main>
+
+<style>
+</style>
