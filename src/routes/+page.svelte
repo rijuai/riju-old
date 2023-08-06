27b80dcc7d8ea29dba8eb3810ec3a133d8373459
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-08-06 11:44:41 +0530

    feat: update home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,28 +2,32 @@
 	import Navbar from '$lib/ui/Navbar.svelte';
 </script>
 
+<svelte:head>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/@picocss/pico@2.0.0-alpha1/css/pico.min.css"
+	/>
+</svelte:head>
+
 <Navbar />
 
 <section class="text-center py-8">
 	<hgroup class="mb-10">
-		<h1>Create Presentation In Seconds!</h1>
-		<p class="mb-6">
-			Riju is the super-fast way to create presentations without the hassle of
-			drag-and-drop, alignment, etc.
-		</p>
+		<h1 class="mb-4">Create Present Share in seconds</h1>
+		<p class="mb-8">Create presentations in 100 seconds.</p>
 		<a href="/" role="button">Sign Up (waiting list)</a>
 		<a class="secondar outline" href="/dashboard" role="button">Dashboard</a>
 	</hgroup>
 
 	<iframe
-		class="h-80 aspect-video"
+		class="h-96 aspect-video"
 		title="as"
 		src="https://customer-m033z5x00ks6nunl.cloudflarestream.com/b236bde30eb07b9d01318940e5fc3eda/iframe?poster=https%3A%2F%2Fcustomer-m033z5x00ks6nunl.cloudflarestream.com%2Fb236bde30eb07b9d01318940e5fc3eda%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600"
 		style="border: none;"
-		allow="autoplay; encrypted-media;"
+		allow="encrypted-media;"
 	/>
 </section>
 
 <style>
-	@import 'https://cdn.jsdelivr.net/npm/@picocss/pico@2.0.0-alpha1/css/pico.min.css';
+	/* @import 'https://cdn.jsdelivr.net/npm/@picocss/pico@2.0.0-alpha1/css/pico.min.css'; */
 </style>
