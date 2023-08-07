2e5b2a54e0769df785c70b7b431b2b508ae88b27
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-08-07 19:53:07 +0530

    feat: create account page

---
+++ src/routes/dashboard/account/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+</script>
+
+<main class="px-4">
+	<article class="card max-w-sm mx-auto">
+		<header>
+			<h1 class="text">Account</h1>
+		</header>
+		<form>
+			<fieldset>
+				<label>
+					Full Name
+					<input
+						type="text"
+						name="Full Name"
+						placeholder="Harish Srinivasan"
+						autocomplete="name"
+					/>
+				</label>
+				<label>
+					Email
+					<input
+						type="email"
+						name="email"
+						placeholder="abc@example.com"
+						autocomplete="email"
+					/>
+				</label>
+			</fieldset>
+		</form>
+		<footer>
+			<input type="submit" value="Save Changes" />
+
+			<p>
+				Facing problems with Riju? <a href="/contact-us">Contact Us</a>
+			</p>
+		</footer>
+	</article>
+</main>
