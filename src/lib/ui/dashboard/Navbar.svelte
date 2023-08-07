e176bd804d0a9e8d99e2ee445676e3cc9f856c16
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-08-07 19:54:09 +0530

    feat: update navbar

--- src/lib/ui/dashboard/Navbar.svelte
+++ src/lib/ui/dashboard/Navbar.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import { LayoutDashboard, PenBox, Play, UserCircle2 } from 'lucide-svelte';
 </script>
 
@@ -6,30 +7,34 @@
 	<ul>
 		<li><a href="/">Riju</a></li>
 		<li>
-			<a class="secondary" href="/dashboard">
-				<UserCircle2 size="24" />
+			<a class="secondary" href="/dashboard/account">
+				<UserCircle2 size="20" />
 			</a>
 		</li>
 		<li>
 			<a class="secondary" href="/dashboard">
-				<LayoutDashboard size="24" />
+				<LayoutDashboard size="20" />
 			</a>
 		</li>
 	</ul>
 	<ul>
-		<li>
-			<a class="secondary outline" href="/">Change Theme</a>
-		</li>
-		<li>
-			<a class="secondary" href="/present">
-				<Play size="24" />
-			</a>
-		</li>
-		<li>
-			<a class="primary" href="/dashboard/presentation" role="button">
-				<PenBox size="24" />
-				Create</a
-			>
-		</li>
+		{#if $page.url.pathname === '/dashboard/presentation'}
+			<li>
+				<a class="secondary outline" href="/">Change Theme</a>
+			</li>
+			<li>
+				<a class="secondary" href="/present">
+					<Play size="20" />
+				</a>
+			</li>
+		{/if}
+		{#if $page.url.pathname === '/dashboard'}
+			<li>
+				<a class="primary" href="/dashboard/presentation" role="button">
+					<PenBox size="20" />
+					Create</a
+				>
+			</li>
+		{/if}
 	</ul>
 </nav>
