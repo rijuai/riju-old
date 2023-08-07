0db85be5e2e228aee87aca3647246c3721ca98b6
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-08-07 19:53:37 +0530

    refactor: add animated route to dashboard layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-	import AnimatedRoute from '$lib/components/AnimatedRoute.svelte';
 	import '../app.css';
 </script>
 
-<AnimatedRoute>
-	<slot />
-</AnimatedRoute>
+<slot />
--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
+	import AnimatedRoute from '$lib/components/AnimatedRoute.svelte';
 	import Navbar from '$lib/ui/dashboard/Navbar.svelte';
 </script>
 
 <Navbar />
 
-<div class="mx-auto px-6 max-w-6xl">
-	<slot />
-</div>
-
-<style>
-	@import 'https://cdn.jsdelivr.net/npm/@picocss/pico@2.0.0-alpha1/css/pico.min.css';
-</style>
+<AnimatedRoute>
+	<main class="px-8 py-2">
+		<slot />
+	</main>
+</AnimatedRoute>
