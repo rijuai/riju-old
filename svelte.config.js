1efbad79e997f96094d193ce2a86704b5139d3df
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-08-09 17:27:16 +0530

    feat: set base path dashboard

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,10 +12,10 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		paths: {
-			base: '/dashboard', // This sets the base URL for your app
-		},
-		// appDir: 'dashboard',
+		// paths: {
+		// 	base: '/dashboard', // This sets the base URL for your app
+		// },
+		appDir: 'dashboard',
 	}
 };
 
