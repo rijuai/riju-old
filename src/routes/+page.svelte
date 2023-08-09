194841c761d03be2f59d1716bc1f374981eb9fd9
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-08-09 22:26:50 +0530

    feat: add home page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+</script>
+
+<section class="p-4">
+	<article class="max-w-xl mx-auto">
+		<header>
+			<iframe
+				class="w-full aspect-video"
+				title="as"
+				src="https://customer-m033z5x00ks6nunl.cloudflarestream.com/b236bde30eb07b9d01318940e5fc3eda/iframe?poster=https%3A%2F%2Fcustomer-m033z5x00ks6nunl.cloudflarestream.com%2Fb236bde30eb07b9d01318940e5fc3eda%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600"
+				style="border: none;"
+				allow="encrypted-media;"
+			/>
+		</header>
+		<hgroup>
+			<h1 class="mb-4">Riju - <i>coming soon</i></h1>
+			<p class="mb-8">Create presentations in 100 seconds.</p>
+			<form role="group">
+				<input type="email" placeholder="Enter your email" />
+				<input type="submit" value="Sign up" />
+			</form>
+		</hgroup>
+	</article>
+</section>
