5ebc4af998172a0cfbf8dc34b186f88b2343abb9
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-08-10 13:04:11 +0530

    feat: update home page ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,29 @@
-<script lang="ts">
-</script>
-
-<section class="p-4">
-	<article class="max-w-xl mx-auto">
+<section class="p-2 lg:p-6">
+	<article class="max-w-md mx-auto">
 		<header>
-			<iframe
-				class="w-full aspect-video"
-				title="as"
-				src="https://customer-m033z5x00ks6nunl.cloudflarestream.com/b236bde30eb07b9d01318940e5fc3eda/iframe?poster=https%3A%2F%2Fcustomer-m033z5x00ks6nunl.cloudflarestream.com%2Fb236bde30eb07b9d01318940e5fc3eda%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600"
-				style="border: none;"
-				allow="encrypted-media;"
-			/>
+			<hgroup>
+				<h1 class="mb-4">Riju - coming soon</h1>
+				<p class="mb-6">Create presentations in seconds</p>
+				<form role="group">
+					<input type="email" placeholder="Enter your email" />
+					<input type="submit" value="Sign up" />
+				</form>
+			</hgroup>
 		</header>
-		<hgroup>
-			<h1 class="mb-4">Riju - <i>coming soon</i></h1>
-			<p class="mb-8">Create presentations in 100 seconds.</p>
-			<form role="group">
-				<input type="email" placeholder="Enter your email" />
-				<input type="submit" value="Sign up" />
-			</form>
-		</hgroup>
+
+		<iframe
+			class="w-full aspect-video"
+			src="https://www.youtube-nocookie.com/embed/Qah9sSIXJqk"
+			title="YouTube video player"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+			allowfullscreen
+		/>
 	</article>
 </section>
+
+<style lang="postcss">
+	:root {
+		@apply bg-black;
+	}
+</style>
