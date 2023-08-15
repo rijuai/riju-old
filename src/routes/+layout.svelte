80d73f071f6db77c3f3f76364879d8c77eca66e4
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-08-15 20:08:09 +0530

    feat: change navbars based of url condition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
+	import { page } from '$app/stores';
+	import DashboardNavbar from '$lib/ui/DashboardNavbar.svelte';
+	import HomePageNavbar from '$lib/ui/HomePageNavbar.svelte';
+	import PresentViewNavbar from '$lib/ui/PresentViewNavbar.svelte';
 	import '../app.css';
 </script>
 
-<main class="mx-auto">
+{#if $page.url.pathname.startsWith('/dashboard')}
+	<DashboardNavbar />
+{:else if $page.url.pathname.startsWith('/present')}
+	<PresentViewNavbar />
+{:else}
+	<HomePageNavbar />
+{/if}
+
+<main class="p-4">
 	<slot />
 </main>
