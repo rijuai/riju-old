9f3078e0adacbfa5d9d99a753c7964b3fd649acc
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-08-16 13:43:19 +0530

    feat: create footer

---
+++ src/lib/ui/Footer.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { Twitter, Youtube } from 'lucide-svelte';
+</script>
+
+<footer class="flex flex-col gap-6">
+	<div class="flex justify-center gap-6">
+		<a href="https://twitter.com/RijuPage">
+			<Twitter />
+		</a>
+		<a href="https://www.youtube.com/channel/UCqCC3zl-WFdSujoAyKANCUg">
+			<Youtube />
+		</a>
+	</div>
+	<div class="flex justify-center gap-4 text-sm">
+		<a class="secondary" href="/privacy">Privacy Policy</a>
+		<a class="secondary" href="/terms">Terms</a>
+	</div>
+</footer>
