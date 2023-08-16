1a4d752dc6c88c979b4887092213ec999d552e4c
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-08-16 17:57:16 +0530

    feat: add contact link to footer

--- src/lib/ui/Footer.svelte
+++ src/lib/ui/Footer.svelte
@@ -2,7 +2,7 @@
 	import { Twitter, Youtube } from 'lucide-svelte';
 </script>
 
-<footer class="flex flex-col gap-6">
+<footer class="flex flex-col gap-6 pb-4">
 	<div class="flex justify-center gap-6">
 		<a href="https://twitter.com/RijuPage">
 			<Twitter />
@@ -12,6 +12,7 @@
 		</a>
 	</div>
 	<div class="flex justify-center gap-4 text-sm">
+		<a class="secondary" href="/contact">Contact</a>
 		<a class="secondary" href="/privacy">Privacy Policy</a>
 		<a class="secondary" href="/terms">Terms</a>
 	</div>
