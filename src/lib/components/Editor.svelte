bda715a288da8fcfc66fea93a4e45ea4b6d1482c
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-08-19 12:09:18 +0530

    feat: store editor content in local storage

--- src/lib/components/Editor.svelte
+++ src/lib/components/Editor.svelte
@@ -29,11 +29,23 @@
 				// force re-render so `editor.isActive` works as expected
 				editor = editor;
 
+				// Store the editor content in local storage
+				localStorage.setItem(
+					'editorContent',
+					JSON.stringify(editor.getJSON().content),
+				);
+
 				console.log('editor', editor.getJSON().content);
 
 				$outputJsonStore = editor.getJSON().content;
 			},
 		});
+
+		if (localStorage.getItem('editorContent')) {
+			editor.commands.setContent(
+				JSON.parse(localStorage.getItem('editorContent') as string),
+			);
+		}
 
 		// Initialize the FileReader once the component is mounted
 		if (fileInput) {
