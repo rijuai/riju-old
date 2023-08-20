b79d7e63758dd3b27b1e4c776f6bc414151e5ba3
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-08-20 17:04:17 +0530

    style: increase padding & reduce font size

--- src/lib/ui/Footer.svelte
+++ src/lib/ui/Footer.svelte
@@ -2,7 +2,7 @@
 	import { IconBrandTwitter, IconBrandYoutube } from '@tabler/icons-svelte';
 </script>
 
-<footer class="flex flex-col gap-6 pb-4">
+<footer class="flex flex-col justify-center gap-6 py-6">
 	<div class="flex justify-center gap-6">
 		<a href="https://twitter.com/RijuPage">
 			<IconBrandTwitter />
@@ -11,7 +11,7 @@
 			<IconBrandYoutube />
 		</a>
 	</div>
-	<div class="flex justify-center gap-4 text-sm">
+	<div class="flex justify-center gap-4 text-xs">
 		<a class="secondary" href="/docs/contact">Contact</a>
 		<a class="secondary" href="/docs/privacy">Privacy Policy</a>
 		<a class="secondary" href="/docs/terms">Terms</a>
