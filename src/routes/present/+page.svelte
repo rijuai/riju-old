0cfe9cca9dedfb4e66efa79a4d99dfcda76503d2
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-08-20 16:27:28 +0530

    chore: add meta tag

--- src/routes/present/+page.svelte
+++ src/routes/present/+page.svelte
@@ -5,6 +5,10 @@
 
 <svelte:head>
 	<title>Riju | Present</title>
+	<meta
+		name="description"
+		content="Create presentations in seconds. Riju turns your words into stunning presentations. Our smart tech blends beauty and impact, perfect for impressing clients or inspiring students."
+	/>
 </svelte:head>
 
 <Reveal content={$outputJsonStore} />
