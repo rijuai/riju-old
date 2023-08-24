847372a36e554b87538bff8f2e5da3bf64a5e093
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-08-24 18:12:24 +0530

    chore: remove wrapper main tag

--- src/routes/dashboard/account/+page.svelte
+++ src/routes/dashboard/account/+page.svelte
@@ -6,23 +6,21 @@
 	<meta name="description" content="Your account details" />
 </svelte:head>
 
-<main class="px-4">
-	<article class="card max-w-sm mx-auto">
-		<header>
-			<h1 class="text">Account</h1>
-		</header>
-		<form>
-			<fieldset>
-				<label>
-					Full name
-					<input type="text" name="Full Name" autocomplete="name" />
-				</label>
-				<label>
-					Email
-					<input type="email" name="email" autocomplete="email" />
-				</label>
-			</fieldset>
-		</form>
-		<input type="submit" value="Save Changes" />
-	</article>
-</main>
+<article class="max-w-sm mx-auto">
+	<header>
+		<h1>Account</h1>
+	</header>
+	<form>
+		<fieldset>
+			<label>
+				Full name
+				<input type="text" name="Full Name" autocomplete="name" />
+			</label>
+			<label>
+				Email
+				<input type="email" name="email" autocomplete="email" />
+			</label>
+		</fieldset>
+	</form>
+	<input type="submit" value="Save Changes" />
+</article>
