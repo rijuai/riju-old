5ea1ba3d56b93069c5e66e74131a391a90e0bd9b
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-08-25 11:20:24 +0530

    chore: remove pico css cdn from login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -8,31 +8,27 @@
 		name="description"
 		content="Create presentations in seconds. Riju turns your words into stunning presentations. Our smart tech blends beauty and impact, perfect for impressing clients or inspiring students."
 	/>
-
-	<!-- Pico CSS -->
-	<link
-		rel="stylesheet"
-		href="https://cdn.jsdelivr.net/npm/@picocss/pico@2.0.0-alpha1/css/pico.min.css"
-	/>
 </svelte:head>
 
-<HomePageNavbar />
+<main class="container-fluid">
+	<HomePageNavbar />
 
-<article class="max-w-sm mx-auto">
-	<header>
-		<h1>Sign up / Login</h1>
-	</header>
-	<form>
-		<fieldset>
-			<label>
-				Email
-				<input type="email" autocomplete="email" aria-label="Email" />
-			</label>
-		</fieldset>
-	</form>
-	<input type="submit" value="Send Magic Link" />
+	<article class="max-w-sm mx-auto">
+		<header>
+			<h1>Sign up / Login</h1>
+		</header>
+		<form>
+			<fieldset>
+				<label>
+					Email
+					<input type="email" autocomplete="email" aria-label="Email" />
+				</label>
+			</fieldset>
+		</form>
+		<input type="submit" value="Send Magic Link" />
 
-	<p>
-		Facing problems? <a href="/docs/contact">Contact</a>
-	</p>
-</article>
+		<p>
+			Facing problems? <a href="/docs/contact">Contact</a>
+		</p>
+	</article>
+</main>
