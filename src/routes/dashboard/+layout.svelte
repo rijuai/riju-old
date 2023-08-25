3fd34148c18b7ba05d0dc3c42b80538bccbd057f
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-08-25 11:21:07 +0530

    chore: remove pico css cdn from dashboard/+layout

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -3,16 +3,6 @@
 	import DashboardNavbar from '$lib/ui/DashboardNavbar.svelte';
 </script>
 
-<svelte:head>
-	<meta name="theme-color" content="#1B1B1B" />
-
-	<!-- Pico CSS -->
-	<link
-		rel="stylesheet"
-		href="https://cdn.jsdelivr.net/npm/@picocss/pico@next/css/pico.min.css"
-	/>
-</svelte:head>
-
 <DashboardNavbar />
 
 <AnimatedRoute>
