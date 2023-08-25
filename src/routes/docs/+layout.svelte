ed9adeb2d8248f4300134cb44e961370323939bb
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-08-25 12:05:22 +0530

    chore: remove pico css cdn link

--- src/routes/docs/+layout.svelte
+++ src/routes/docs/+layout.svelte
@@ -3,16 +3,6 @@
 	import HomePageNavbar from '$lib/ui/HomePageNavbar.svelte';
 </script>
 
-<svelte:head>
-	<meta name="theme-color" content="#1B1B1B" />
-
-	<!-- Pico CSS -->
-	<link
-		rel="stylesheet"
-		href="https://cdn.jsdelivr.net/npm/@picocss/pico@next/css/pico.min.css"
-	/>
-</svelte:head>
-
 <HomePageNavbar />
 
 <slot />
