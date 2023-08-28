6073973e61dbe9ea815b21ff3165ebd8bb73d8db
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-08-28 22:47:24 +0530

    chore: remove demo cards data

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,16 +1,5 @@
 <script lang="ts">
-	let presentations = [
-		{
-			title: 'Riju - Create presentations in 100 seconds',
-			img_src:
-				'https://pub-4b7e6ea7988748b6a1371a8dac387999.r2.dev/‎presentation-1.‎001.jpeg',
-		},
-		{
-			title: 'Riju- Sales Pitch',
-			img_src:
-				'https://pub-4b7e6ea7988748b6a1371a8dac387999.r2.dev/‎presentation-2.‎001.jpeg',
-		},
-	];
+	let presentations: string[];
 </script>
 
 <svelte:head>
@@ -18,15 +7,4 @@
 	<meta name="description" content="Your presentations" />
 </svelte:head>
 
-<section>
-	<div class="grid grid-cols-5 gap-6">
-		{#each presentations as presentation}
-			<a class="rounded-md no-underline" href="/dashboard/presentation">
-				<img class="w-full rounded-md" src={presentation.img_src} alt="" />
-				<p class="text-xs font-semibold truncate text-zinc-400 pt-4">
-					{presentation.title}
-				</p>
-			</a>
-		{/each}
-	</div>
-</section>
+<section />
