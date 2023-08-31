e6dfc2fbb508ca131b433cb24351db0baef1e179
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-08-31 22:36:21 +0530

    style: add lucide icons to presenter navbar

--- src/lib/ui/PresenterNavbar.svelte
+++ src/lib/ui/PresenterNavbar.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { changeTheme } from '$lib/stores/presenter';
+	import { Sparkles, X } from 'lucide-svelte';
 </script>
 
 <nav class="container-fluid fixed z-50 w-full">
@@ -11,44 +12,11 @@
 	<ul>
 		<li>
 			<a class="secondary" href="#change-theme" on:click={changeTheme}
-				><svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="icon icon-tabler icon-tabler-sparkles"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					stroke-width="2"
-					stroke="currentColor"
-					fill="none"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-				>
-					<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-					<path
-						d="M16 18a2 2 0 0 1 2 2a2 2 0 0 1 2 -2a2 2 0 0 1 -2 -2a2 2 0 0 1 -2 2zm0 -12a2 2 0 0 1 2 2a2 2 0 0 1 2 -2a2 2 0 0 1 -2 -2a2 2 0 0 1 -2 2zm-7 12a6 6 0 0 1 6 -6a6 6 0 0 1 -6 -6a6 6 0 0 1 -6 6a6 6 0 0 1 6 6z"
-					/>
-				</svg>Theme</a
+				><Sparkles /> Theme</a
 			>
 		</li>
 		<li>
-			<a class="secondary" href="/dashboard/editor"
-				><svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="icon icon-tabler icon-tabler-x"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					stroke-width="2"
-					stroke="currentColor"
-					fill="none"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-				>
-					<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-					<path d="M18 6l-12 12" />
-					<path d="M6 6l12 12" />
-				</svg></a
-			>
+			<a class="secondary" href="/dashboard/editor"><X /></a>
 		</li>
 	</ul>
 </nav>
