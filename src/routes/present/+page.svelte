8111745c8e7f16f4e6e741612f567257ae87d677
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-08-31 22:44:51 +0530

    refactor: stop passing content prop to Reveal component

--- src/routes/present/+page.svelte
+++ src/routes/present/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import Reveal from '$lib/components/Presenter.svelte';
-	import { editorOutput } from '$lib/stores/editor';
 	import PresenterNavbar from '$lib/ui/PresenterNavbar.svelte';
 </script>
 
@@ -14,4 +13,4 @@
 
 <PresenterNavbar />
 
-<Reveal content={$editorOutput} />
+<Reveal />
