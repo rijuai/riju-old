0fbdc788d3c26cd1789a01ed9064f7844ab31d6b
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-08-31 22:22:34 +0530

    style: update dashboard menu icons to lucide icons

--- src/lib/ui/DashboardNavbar.svelte
+++ src/lib/ui/DashboardNavbar.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import { PencilLine, Play } from 'lucide-svelte';
 </script>
 
 <nav class="container-fluid">
@@ -10,26 +11,7 @@
 		{#if $page.url.pathname === '/dashboard/editor'}
 			<li>
 				<a class="secondary" href="/present" title="Present">
-					<!-- Present Icon -->
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="icon icon-tabler icon-tabler-player-play-filled"
-						width="24"
-						height="24"
-						viewBox="0 0 24 24"
-						stroke-width="2"
-						stroke="currentColor"
-						fill="none"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-					>
-						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-						<path
-							d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z"
-							stroke-width="0"
-							fill="currentColor"
-						/>
-					</svg>
+					<Play />
 				</a>
 			</li>
 		{/if}
@@ -41,24 +23,7 @@
 					role="button"
 					title="Create Presentation"
 				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="icon icon-tabler icon-tabler-pencil-plus"
-						width="24"
-						height="24"
-						viewBox="0 0 24 24"
-						stroke-width="2"
-						stroke="currentColor"
-						fill="none"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-					>
-						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-						<path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" />
-						<path d="M13.5 6.5l4 4" />
-						<path d="M16 19h6" />
-						<path d="M19 16v6" />
-					</svg>
+					<PencilLine />
 					Create</a
 				>
 			</li>
