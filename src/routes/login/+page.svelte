fc16b5a72360e8a9f23b6a07ec3f792e7dd38235
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-09-05 12:33:41 +0530

    feat: implement login functionality using magic link

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,27 @@
 <script lang="ts">
-	import HomePageNavbar from '$lib/ui/HomePageNavbar.svelte';
+	import { supabase } from '$lib/config/supabase'
+	import HomePageNavbar from '$lib/ui/HomePageNavbar.svelte'
+
+	let email: string
+	let emailSent = false
+
+	const signInWithMagicLink = async (email: string) => {
+		let { data, error } = await supabase.auth.signInWithOtp({
+			email: email,
+			options: {
+				emailRedirectTo: 'https://riju.page/dashboard?userLoggedIn=true',
+			},
+		})
+
+		if (error) {
+			console.log(error)
+			return
+		} else {
+			console.log('SUCCESS')
+			console.log(data)
+			emailSent = true
+		}
+	}
 </script>
 
 <svelte:head>
@@ -12,23 +34,46 @@
 
 <main class="container-fluid">
 	<HomePageNavbar />
-
-	<article class="max-w-sm mx-auto">
-		<header>
-			<h1>Sign up / Login</h1>
-		</header>
-		<form>
-			<fieldset>
-				<label>
-					Email
-					<input type="email" autocomplete="email" aria-label="Email" />
-				</label>
-			</fieldset>
-		</form>
-		<input type="submit" value="Send Magic Link" />
-
-		<p>
-			Facing problems? <a href="/docs/contact">Contact</a>
-		</p>
-	</article>
+	{#if emailSent === false}
+		<article class="max-w-sm mx-auto">
+			<header>
+				<h1>Sign up / Login</h1>
+			</header>
+			<form
+				on:submit={() => {
+					signInWithMagicLink(email)
+				}}
+			>
+				<fieldset>
+					<label>
+						Email
+						<input
+							type="email"
+							autocomplete="email"
+							aria-label="Email"
+							bind:value={email}
+						/>
+					</label>
+					<input
+						type="submit"
+						value="Continue"
+						aria-busy={emailSent}
+						aria-label="Please wait…"
+					/>
+					<small>
+						By continuing, you agree to our <a href="/docs/terms">Terms</a> and
+						<a href="/docs/privacy">Privacy Policy</a>.</small
+					>
+				</fieldset>
+				<p>
+					Facing problems? <a href="/docs/contact">Contact</a>
+				</p>
+			</form>
+		</article>
+	{:else}
+		<article class="max-w-sm mx-auto">
+			<header>Note</header>
+			<p>Click on the link sent to your email.</p>
+		</article>
+	{/if}
 </main>
