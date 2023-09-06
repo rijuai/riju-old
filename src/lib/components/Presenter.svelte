1ec85aa32bee9c592748669c6e8dfac0b82d066b
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-09-06 12:42:52 +0530

    refactor: move presenter (content to html) logic to separate engines folder

--- src/lib/components/Presenter.svelte
+++ src/lib/components/Presenter.svelte
@@ -1,73 +1,37 @@
 <script lang="ts">
-	import '$lib/assets/presenter.css';
-	import { editorOutput } from '$lib/stores/editor';
-	import { currentTheme } from '$lib/stores/presenter';
+	import { convertContentToHtml } from '$lib/engines/convertContentToHtml'
 
-	import Reveal from 'reveal.js';
-	import 'reveal.js/dist/reveal.css';
-	import { onMount } from 'svelte';
+	import '$lib/assets/presenter.css'
+	import { editorOutput } from '$lib/stores/editor'
+	import { currentTheme } from '$lib/stores/presenter'
 
-	let deck: Reveal.Api;
-	let content = $editorOutput;
-	let htmlOutput = '<section>';
+	import Reveal from 'reveal.js'
+	import 'reveal.js/dist/reveal.css'
+	import { onMount } from 'svelte'
+
+	let deck: Reveal.Api
+	let content = $editorOutput
+	let htmlOutput = '<section>'
 
 	onMount(() => {
-		deck = new Reveal();
+		deck = new Reveal()
 
 		deck.initialize({
 			controls: false,
 			progress: true,
 			history: false,
 			center: true,
-			transition: 'slide',
+			transition: 'fade',
 			embedded: true,
 			autoAnimate: true,
-			backgroundTransition: 'slide',
 			overview: false,
-		});
-	});
+		})
+	})
 
-	if (content !== undefined) {
-		content.forEach(
-			(item: {
-				type: string;
-				content: any[];
-				attrs: { start: number; src: string };
-				text: string;
-			}) => {
-				if (item.type === 'paragraph' && item.content === undefined) {
-					htmlOutput += `</section><section>`;
-				} else if (item.type === 'heading') {
-					htmlOutput += `<h1>${item.content[0].text}</h1>`;
-				} else if (item.type === 'paragraph') {
-					htmlOutput += `<p>${item.content[0].text}</p>`;
-				} else if (item.type === 'orderedList') {
-					htmlOutput += `<ol start=${item.attrs.start}>`;
-					item.content.forEach(
-						(listItem: { content: { content: { text: string }[] }[] }) => {
-							htmlOutput += `<li>${listItem.content[0].content[0].text}</li>`;
-						},
-					);
-					htmlOutput += `</ol>`;
-				} else if (item.type === 'bulletList') {
-					htmlOutput += `<ul>`;
-					item.content.forEach(
-						(listItem: { content: { content: { text: string }[] }[] }) => {
-							htmlOutput += `<li>${listItem.content[0].content[0].text}</li>`;
-						},
-					);
-					htmlOutput += `</ul>`;
-				} else if (item.type === 'image') {
-					htmlOutput += `<img src=${item.attrs.src} />`;
-				} else {
-					htmlOutput += `<p>${item.text}</p>`;
-				}
-			},
-		);
-	}
+	if (content !== undefined) htmlOutput = convertContentToHtml(content)
 </script>
 
-<main class="reveal" style={$currentTheme}>
+<main class="reveal w-full aspect-video" style={$currentTheme}>
 	<div class="slides">
 		{@html htmlOutput}
 	</div>
