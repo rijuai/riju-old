fff1728f5346646e87f51930b9528ebe66e9be56
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-09-16 12:18:27 +0530

    feat(db): add presentation db queries

---
+++ src/lib/db/presentation.ts
@@ -0,0 +1,56 @@
+import { supabase } from '$lib/config/supabase'
+import type { JSONContent } from '@tiptap/core'
+
+export const getPresentations = async () => {
+	const { data, error } = await supabase
+		.from('presentations')
+		.select('presentation_id, title, preview_img_url')
+		.order('updated_at', { ascending: false })
+
+	if (error) return
+
+	console.log('DATA', data)
+	return data
+}
+
+export const createPresentation = async (
+	userId: string,
+	title: string,
+): Promise<string> => {
+	const { data, error } = await supabase
+		.from('presentations')
+		.insert({
+			user_id: userId,
+			title: title,
+		})
+		.select('presentation_id')
+
+	if (error) console.log(error)
+
+	return data![0].presentation_id
+}
+
+export const getPresentationContent = async (
+	presentation_id: string,
+): Promise<{ content: any } | undefined> => {
+	const { data, error } = await supabase
+		.from('presentations')
+		.select('content')
+		.eq('presentation_id', presentation_id)
+
+	if (error) return
+
+	return data[0]
+}
+
+export const updatePresentation = async (
+	presentation_id: string,
+	content: JSONContent,
+) => {
+	const { error } = await supabase
+		.from('presentations')
+		.update({ content: content })
+		.eq('presentation_id', presentation_id)
+
+	if (error) return console.log(error)
+}
