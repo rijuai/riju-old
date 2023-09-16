8904564b89051b75b86db63f92660e0633e6c74b
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-09-16 21:27:18 +0530

    feat: add auth wrapper to dashboard layout

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,12 +1,15 @@
 <script>
 	import AnimatedRoute from '$lib/components/AnimatedRoute.svelte'
+	import Auth from '$lib/components/Auth.svelte'
 	import DashboardNavbar from '$lib/ui/DashboardNavbar.svelte'
 </script>
 
 <DashboardNavbar />
 
-<AnimatedRoute>
-	<main class="container-fluid">
-		<slot />
-	</main>
-</AnimatedRoute>
+<Auth>
+	<AnimatedRoute>
+		<main class="container-fluid">
+			<slot />
+		</main>
+	</AnimatedRoute>
+</Auth>
