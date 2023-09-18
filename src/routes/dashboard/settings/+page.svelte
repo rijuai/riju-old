4515da93dec2cb49a6f4468bfb0088efd60f5903
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-09-18 15:22:21 +0530

    feat(settings): add email field,and use goto when signout

--- src/routes/dashboard/settings/+page.svelte
+++ src/routes/dashboard/settings/+page.svelte
@@ -1,21 +1,36 @@
 <script lang="ts">
+	import { goto } from '$app/navigation'
 	import { supabase } from '$lib/config/supabase'
+	import { getUserData } from '$lib/db/user'
+	import { onMount } from 'svelte'
+
+	let email: string
+
+	onMount(async () => {
+		let result = await getUserData()
+		if (result) email = result.email!
+	})
 </script>
 
 <section>
-	<article class="mx-auto max-w-md">
+	<article class="mx-auto max-w-sm">
 		<header>
 			<h1>Settings</h1>
 		</header>
 		<fieldset>
-			<button
-				class="secondary"
-				on:click={async () => {
-					await supabase.auth.signOut()
-
-					window.location.href = '/'
-				}}>Sign Out</button
-			>
+			<label
+				>Email
+				<input type="email" aria-label="Email" disabled bind:value={email} />
+			</label>
+			<div class="flex justify-end">
+				<button
+					class="secondary"
+					on:click={async () => {
+						await supabase.auth.signOut()
+						goto('/')
+					}}>Sign Out</button
+				>
+			</div>
 		</fieldset>
 	</article>
 </section>
