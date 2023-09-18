9c6f4f41a4e360cc70d1e9c043deeed827d3c84b
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-09-18 15:42:37 +0530

    chore(query): add console.error

--- src/lib/db/user.ts
+++ src/lib/db/user.ts
@@ -3,7 +3,6 @@ import { supabase } from '$lib/config/supabase'
 export const getUserData = async () => {
 	const { data, error } = await supabase.auth.getSession()
 
-	if (error) return
-
+	if (error) return console.error(error)
 	return data.session!.user
 }
