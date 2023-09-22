3a32fc333414c46f7faec62550a59b6b2fbe1eba
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-09-22 17:05:33 +0530

    feat(editor): add image support fixes #22

--- src/lib/components/EditorMenu.svelte
+++ src/lib/components/EditorMenu.svelte
@@ -1,9 +1,47 @@
 <script lang="ts">
 	import { showDeleteModal } from '$lib/stores/editor'
 	import type { Editor } from '@tiptap/core'
-	import { Heading1, Heading2, List, ListOrdered, Trash2 } from 'lucide-svelte'
+	import {
+		Heading1,
+		Heading2,
+		ImageIcon,
+		List,
+		ListOrdered,
+		Trash2,
+	} from 'lucide-svelte'
+	import { onMount } from 'svelte'
 
 	export let editor: Editor
+	let fileInput: HTMLInputElement
+	let imageUrl: string
+
+	function handleImage(event: Event) {
+		const target = event.target as HTMLInputElement
+		if (!target.files || target.files.length === 0) return
+
+		const file = target.files[0]
+		const reader = new FileReader()
+
+		reader.onloadend = () => {
+			imageUrl = reader.result as string
+			const transaction = editor
+				.chain()
+				.focus()
+				.setImage({ src: imageUrl })
+				.createParagraphNear()
+				.run()
+			fileInput.value = ''
+		}
+
+		reader.readAsDataURL(file)
+	}
+
+	onMount(() => {
+		// Initialize the FileReader once the component is mounted
+		if (fileInput) {
+			fileInput.addEventListener('change', handleImage)
+		}
+	})
 </script>
 
 <div class="menu z-50 flex flex-col gap-2 p-1 rounded-md">
@@ -37,23 +75,22 @@
 			editor.chain().focus().toggleOrderedList().run()
 		}}><ListOrdered /></button
 	>
+	<input
+		type="file"
+		id="imageInput"
+		style="display: none;"
+		bind:this={fileInput}
+		on:change={handleImage}
+	/>
+	<button class="secondary" on:click={() => fileInput.click()}
+		><ImageIcon /></button
+	>
 	<button
 		class="secondary"
 		on:click={() => {
 			$showDeleteModal = true
 		}}><Trash2 /></button
 	>
-
-	<!-- <input
-		type="file"
-		id="imageInput"
-		style="display: none;"
-		bind:this={fileInput}
-		on:change={handleFileInput}
-	/>
-	<button class="secondary" on:click={() => fileInput.click()}
-		><ImageIcon /></button
-	> -->
 </div>
 
 <style lang="postcss">
