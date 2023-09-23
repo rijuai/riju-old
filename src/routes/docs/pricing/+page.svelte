7034472e9d6b16d29227032375ba6cd66fa7e7bc
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-09-23 16:58:22 +0530

    feat: create pricing page fix #29

---
+++ src/routes/docs/pricing/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<section>
+	<article class="max-w-sm mx-auto">
+		<header>
+			<h1>Pricing</h1>
+		</header>
+		<ul>
+			<li>Currently, Riju is in beta and is free to use.</li>
+		</ul>
+	</article>
+</section>
