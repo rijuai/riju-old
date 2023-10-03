679e7c75476e4b82bde1657549dd19a4cbe06aa2
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-10-03 21:46:58 +0530

    feat: update banner text

--- src/lib/ui/Banner.svelte
+++ src/lib/ui/Banner.svelte
@@ -1,5 +1,6 @@
 <div class="w-full p-2" style="background-color: #00895A;">
 	<small class="text-slate-200 text-center mb-0 flex justify-center">
-		Note: Riju is still in beta so, you may experience bugs, sorry for that.
+		Riju is still in alpha so, you may experience bugs. Lot more AI themes, more
+		features coming soon.
 	</small>
 </div>
