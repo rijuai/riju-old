f5a78a9c93860ffd526c324c352e58045be80676
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-10-03 21:49:41 +0530

    feat: update text in pricing page

--- src/routes/docs/pricing/+page.svelte
+++ src/routes/docs/pricing/+page.svelte
@@ -7,7 +7,7 @@
 			<h1>Pricing</h1>
 		</header>
 		<ul>
-			<li>Currently, Riju is in beta and is free to use.</li>
+			<li>Currently, Riju is in alpha and is free to use.</li>
 		</ul>
 	</article>
 </section>
