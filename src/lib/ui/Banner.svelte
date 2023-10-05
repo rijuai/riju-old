4b9e8dc89a1fc1e23bed26c5b3e15c0a87b7484f
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-10-05 12:39:21 +0530

    style(banner): update background color

--- src/lib/ui/Banner.svelte
+++ src/lib/ui/Banner.svelte
@@ -1,5 +1,8 @@
-<div class="w-full p-2" style="background-color: #00895A;">
-	<small class="text-slate-200 text-center mb-0 flex justify-center">
+<div
+	class="w-full p-2"
+	style="background-color: var(--pico-secondary-background);"
+>
+	<small class="text-center mb-0 flex justify-center text-white">
 		Riju is still in alpha so, you may experience bugs. Lot more AI themes, more
 		features coming soon.
 	</small>
