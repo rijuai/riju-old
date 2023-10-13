ca7af64470117f4d5e170f0bcea695045a45b4f5
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-10-13 12:49:25 +0530

    feat: create api for creating presentation

---
+++ src/routes/api/presentation/+server.ts
@@ -0,0 +1,30 @@
+import { supabase } from '$lib/config/supabase'
+import { json } from '@sveltejs/kit'
+import type { JSONContent } from '@tiptap/core'
+
+export const POST = async ({ request }) => {
+	const body: {
+		title: string
+		content?: JSONContent
+	} = await request.json()
+
+	const { data, error } = await supabase
+		.from('public_presentations')
+		.insert({
+			title: body.title,
+			content: body.content,
+		})
+		.select('presentation_id')
+
+	if (error) {
+		console.log(error)
+		return json({
+			success: false,
+			error: error.message,
+		})
+	}
+
+	return json({
+		presentation_id: data![0].presentation_id,
+	})
+}
