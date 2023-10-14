0d2b7ad016ce6b47c29d025ceed11543d36b20f5
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-10-14 10:28:24 +0530

    feat: add tooltips for editor menus fix #49

--- src/lib/components/EditorMenu.svelte
+++ src/lib/components/EditorMenu.svelte
@@ -10,32 +10,43 @@
 	class="menu z-50 flex flex-col gap-2 p-1 rounded-md fixed left-4 top-1/2 transform -translate-y-1/2 bg-zinc-100"
 >
 	<button
-		class="fill-red-500 text-red-500 bg-red-600"
+		data-tooltip="Heading 1"
+		data-placement="right"
 		on:click={() => {
 			editor.chain().focus().toggleHeading({ level: 1 }).run()
 		}}><Heading1 /></button
 	>
 	<button
+		data-tooltip="Heading 2"
+		data-placement="right"
 		on:click={() => {
 			editor.chain().focus().toggleHeading({ level: 2 }).run()
 		}}><Heading2 /></button
 	>
 	<button
+		data-tooltip="Paragraph"
+		data-placement="right"
 		on:click={() => {
 			editor.chain().focus().setParagraph().run()
 		}}>P</button
 	>
 	<button
+		data-tooltip="Bullet List"
+		data-placement="right"
 		on:click={() => {
 			editor.chain().focus().toggleBulletList().run()
 		}}><List /></button
 	>
 	<button
+		data-tooltip="Ordered List"
+		data-placement="right"
 		on:click={() => {
 			editor.chain().focus().toggleOrderedList().run()
 		}}><ListOrdered /></button
 	>
 	<button
+		data-tooltip="Delete Presentation"
+		data-placement="right"
 		on:click={() => {
 			$showDeleteModal = true
 		}}><Trash2 /></button
@@ -44,9 +55,7 @@
 
 <style lang="postcss">
 	.menu button {
-		@apply bg-zinc-100 border-8 border-zinc-600;
-		border-width: 0;
-		color: #373c44;
+		@apply bg-zinc-100 text-zinc-800 border-0;
 	}
 
 	.menu button:hover,
