a8934dae960ef844db87badd59796b73f362f127
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-10-14 13:20:29 +0530

    refactor: undo last commit changes

--- src/routes/api/presentation/+server.ts
+++ src/routes/api/presentation/+server.ts
@@ -1,13 +1,6 @@
-// import { supabase } from '$lib/config/supabase'
-import { createClient } from '@supabase/supabase-js'
+import { supabase } from '$lib/config/supabase'
 import { json } from '@sveltejs/kit'
 import type { JSONContent } from '@tiptap/core'
-
-const SUPABSE_URL = 'https://xrqtyfvyqmwhdnuyskip.supabase.co'
-const SUPABASE_ANON_KEY =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhycXR5ZnZ5cW13aGRudXlza2lwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI1MTIzMDUsImV4cCI6MjAwODA4ODMwNX0.fS48YgFiPMHbyunud5vQO3lAe3maHOAlJyn-_-3ZuWg'
-
-const supabase = createClient(SUPABSE_URL, SUPABASE_ANON_KEY)
 
 export const POST = async ({ request }) => {
 	const body: {
