1c6a61e47dc640d0c5900fbc8069c62e1ce79ede
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-10-19 12:52:05 +0530

    style(banner): reduce intensity of bg colour

--- src/lib/ui/Banner.svelte
+++ src/lib/ui/Banner.svelte
@@ -2,7 +2,7 @@
 	export let message: string
 </script>
 
-<div class="w-full p-2 bg-amber-300">
+<div class="w-full p-2 bg-amber-200">
 	<small class="text-center mb-0 flex justify-center text-zinc-800">
 		{message}
 	</small>
