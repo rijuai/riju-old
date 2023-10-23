3445d6ab936906ecfbc054b9dd7ade11d6795eaa
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-10-23 10:27:01 +0530

    style(footer): update font size and spacing fix #57

--- src/lib/ui/Footer.svelte
+++ src/lib/ui/Footer.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
-	import { Twitter, Youtube } from 'lucide-svelte'
+	import Separator from '$lib/components/ui/separator/separator.svelte'
+	import { Linkedin, Twitter, Youtube } from 'lucide-svelte'
 </script>
 
-<footer class="flex flex-col justify-center gap-6 py-6">
+<footer class="flex flex-col justify-center gap-6 py-8">
 	<div class="flex justify-center gap-6">
 		<a href="https://twitter.com/RijuPage"><Twitter /></a>
 		<a href="https://www.youtube.com/channel/UCqCC3zl-WFdSujoAyKANCUg"
 			><Youtube />
 		</a>
+		<a href="/"><Linkedin /></a>
 	</div>
-	<div class="flex justify-center gap-4 text-xs">
-		<a class="secondary" href="/docs/contact">Contact</a>
-		<a class="secondary" href="/docs/privacy">Privacy Policy</a>
-		<a class="secondary" href="/docs/terms">Terms</a>
+	<div class="flex justify-center gap-6 text-xs text-muted-foreground">
+		<a href="/docs/contact">Contact</a>
+		<a href="/docs/privacy">Privacy Policy</a>
+		<a href="/docs/terms">Terms</a>
 	</div>
 </footer>
