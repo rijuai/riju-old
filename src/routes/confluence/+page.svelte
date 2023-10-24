b4428cc6a75816dfe8714b7a7eaf682dbed1ca74
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-10-24 23:38:55 +0530

    fix:

--- src/routes/confluence/+page.svelte
+++ src/routes/confluence/+page.svelte
@@ -2,17 +2,18 @@
 	import MetaData from '$lib/components/MetaData.svelte'
 	import { page } from '$app/stores'
 	import { currentTheme } from '$lib/stores/presenter'
-	import { Editor, type JSONContent } from '@tiptap/core'
 	import Reveal from 'reveal.js'
 	import 'reveal.js/dist/reveal.css'
 	import { onMount } from 'svelte'
 	import { supabase } from '$lib/config/supabase'
 	import PresenterNavbar from '$lib/ui/PresenterNavbar.svelte'
 	import WaterMark from '$lib/ui/WaterMark.svelte'
+	import { convertContentToHtml } from '$lib/engines/convertContentToHtml'
+	import type { JSONContent } from '@tiptap/core/dist/packages/core/src/types'
 
 	export let presentationId: string
 
-	let reveal: Reveal.Api, htmlOutput: string, presentationContent: string
+	let reveal: Reveal.Api, htmlOutput: string, presentationContent: JSONContent
 
 	const getPresentationId = () => {
 		const id = $page.url.searchParams.get('id') ?? ''
@@ -28,23 +29,14 @@
 			.eq('presentation_id', presentation_id)
 
 		if (error) return console.log(error)
-		return data![0].content
-	}
-
-	const convertContentToText = async (content: string) => {
-		let editor = new Editor({
-			onCreate: async () => {
-				editor.commands.setContent(content)
-			},
-		})
-
-		let text = editor.getText()
-		return text
+		return data![0]
 	}
 
 	onMount(async () => {
 		presentationId = getPresentationId()
 		presentationContent = await getPresentationContent(presentationId!)
+		if (presentationContent)
+			htmlOutput = convertContentToHtml(presentationContent)
 
 		if (presentationContent) reveal = new Reveal()
 
@@ -58,8 +50,6 @@
 			autoAnimate: true,
 			overview: false,
 		})
-
-		let text = convertContentToText(presentationContent)
 	})
 </script>
 
