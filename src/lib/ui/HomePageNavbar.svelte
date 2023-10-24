9bf636d596a0553bdf081676bc78b1cbe51faf20
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-10-24 10:55:16 +0530

    style(homepage nav): update padding

--- src/lib/ui/HomePageNavbar.svelte
+++ src/lib/ui/HomePageNavbar.svelte
@@ -1,27 +1,16 @@
 <script lang="ts">
 	import Button from '$lib/components/ui/button/button.svelte'
-	import { Menu } from 'lucide-svelte'
 	import { onMount } from 'svelte'
-
-	let isMobile: boolean
-	let deviceWidth = window.innerWidth
-
-	onMount(() => {
-		window.addEventListener('resize', () => {
-			deviceWidth = window.innerWidth
-			isMobile = deviceWidth < 768
-		})
-	})
 </script>
 
-<nav class="flex justify-between p-2">
+<nav class="flex justify-between p-2.5">
 	<ul>
 		<li><Button variant="link" class="text-2xl" href="/">Riju.ai</Button></li>
 	</ul>
 	<ul class="flex gap-1 items-center">
 		<li><Button variant="link" href="/docs/contact">Contact</Button></li>
 		<li>
-			<Button variant="outline" href="/dashboard" role="button"
+			<Button variant="secondary" href="/dashboard" role="button"
 				>Dashboard</Button
 			>
 		</li>
