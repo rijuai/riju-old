4fe3061a890f8ce564faccc558982fc5f19dcbce
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-10-24 23:27:26 +0530

    feat(confluence presenter): update navbar

--- src/routes/confluence/+page.svelte
+++ src/routes/confluence/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import MetaData from '$lib/components/MetaData.svelte'
-	import { changeTheme } from '$lib/stores/presenter'
 	import { page } from '$app/stores'
 	import { currentTheme } from '$lib/stores/presenter'
 	import { Editor, type JSONContent } from '@tiptap/core'
@@ -8,7 +7,8 @@
 	import 'reveal.js/dist/reveal.css'
 	import { onMount } from 'svelte'
 	import { supabase } from '$lib/config/supabase'
-	import { Expand, Sparkles, X } from 'lucide-svelte'
+	import PresenterNavbar from '$lib/ui/PresenterNavbar.svelte'
+	import WaterMark from '$lib/ui/WaterMark.svelte'
 
 	export let presentationId: string
 
@@ -31,31 +31,6 @@
 		return data![0].content
 	}
 
-	const showFullScreen = (element: Element) => {
-		const requestFullScreen =
-			element.requestFullscreen ||
-			(element as any).mozRequestFullScreen ||
-			(element as any).webkitRequestFullScreen ||
-			(element as any).msRequestFullscreen
-
-		requestFullScreen.call(element)
-	}
-
-	let log: string
-	const summariseContent = async (content: string): Promise<string | void> => {
-		let response = await fetch('/api/summarise', {
-			method: 'POST',
-			body: JSON.stringify({ content: content }),
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		})
-
-		let data = await response.json()
-		log = JSON.stringify(data)
-		console.log(data)
-	}
-
 	const convertContentToText = async (content: string) => {
 		let editor = new Editor({
 			onCreate: async () => {
@@ -71,10 +46,7 @@
 		presentationId = getPresentationId()
 		presentationContent = await getPresentationContent(presentationId!)
 
-		if (presentationContent)
-			// htmlOutput = convertContentToHtml(JSON.parse(presentationContent))
-
-			reveal = new Reveal()
+		if (presentationContent) reveal = new Reveal()
 
 		reveal.initialize({
 			controls: false,
@@ -96,60 +68,7 @@
 	description="Create presentations in seconds. Riju turns your words into stunning presentations. Our smart tech blends beauty and impact, perfect for impressing clients or inspiring students."
 />
 
-<nav class="container-fluid w-full">
-	<ul>
-		<li class="text-xl">
-			<a class="text-xl" href="/">Riju</a>
-		</li>
-	</ul>
-	<ul>
-		<li>
-			<button
-				class="secondary"
-				on:click={() => {
-					summariseContent(presentationContent)
-				}}>Summarise</button
-			>
-		</li>
-		<li>
-			<a
-				class="secondary"
-				data-tooltip="Change Theme"
-				data-placement="bottom"
-				href="#change_theme=true"
-				on:click={changeTheme}><Sparkles /> Theme</a
-			>
-		</li>
-		<li>
-			<a
-				class="secondary"
-				data-tooltip="View Fullscreen"
-				data-placement="bottom"
-				href="#full_screen=true"
-				on:click={() => {
-					let element = document.querySelector('.reveal')
-
-					if (element !== null) {
-						showFullScreen(element)
-					}
-				}}><Expand /></a
-			>
-		</li>
-		<li>
-			<!-- svelte-ignore a11y-click-events-have-key-events -->
-			<!-- svelte-ignore a11y-no-static-element-interactions -->
-			<!-- svelte-ignore a11y-missing-attribute -->
-			<a
-				class="secondary cursor-pointer"
-				data-tooltip="Exit"
-				data-placement="bottom"
-				on:click={() => {
-					history.back()
-				}}><X /></a
-			>
-		</li>
-	</ul>
-</nav>
+<PresenterNavbar />
 
 <main class="reveal w-full aspect-video" style={$currentTheme}>
 	<div class="slides">
@@ -157,7 +76,5 @@
 	</div>
 	<div id="editor" />
 
-	<small class="bg-white rounded-md fixed bottom-4 right-4 p-2">
-		Created in Riju
-	</small>
+	<WaterMark text="Created in Riju.ai" />
 </main>
