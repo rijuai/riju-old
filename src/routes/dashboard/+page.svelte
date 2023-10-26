d58f3f3b20a37aa46f60671fad214db10dbd4e0a
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-10-26 18:03:49 +0530

    chore(dashboard): update height & font size

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -30,17 +30,14 @@
 	{#if presentations !== undefined && presentations.length > 0}
 		<div class="grid grid-cols-5 gap-8">
 			{#each presentations as presentation}
-				<a
-					class="contrast no-underline"
-					href="/dashboard/editor?id={presentation.presentation_id}"
-				>
-					<div class="w-full h-24 bg-zinc-200 rounded mb-2" />
-					<small class="block truncate">{presentation.title}</small>
+				<a href="/dashboard/editor?id={presentation.presentation_id}">
+					<div class="w-full h-32 bg-zinc-200 rounded mb-2.5" />
+					<p class="block truncate text-[14px]">{presentation.title}</p>
 				</a>
 			{/each}
 		</div>
 	{:else}
-		<p class="max-w-sm mx-auto text-center">
+		<p class="max-w-md mx-auto text-center text-sm">
 			No presentations found. Click on the "Create" button to create a new
 			presentation.
 		</p>
