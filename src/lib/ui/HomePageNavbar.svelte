f2d82a15c30bf3265ee181ed1f524036352c9498
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-11-04 21:13:20 +0530

    feat: redesign homepage fix #80

--- src/lib/ui/HomePageNavbar.svelte
+++ src/lib/ui/HomePageNavbar.svelte
@@ -3,9 +3,10 @@
 </script>
 
 <nav class="flex justify-between p-2.5">
-	<Button variant="link" class="text-2xl" href="/">Riju.ai</Button>
+	<Button variant="link" class="text-2xl" href="/">Riju</Button>
 	<div class="flex gap-1.5 items-center">
 		<Button variant="link" href="/docs/contact">Contact</Button>
-		<Button variant="secondary" href="/dashboard">Dashboard</Button>
+		<Button variant="secondary" href="/login">Sign In</Button>
+		<Button href="/login">Get started</Button>
 	</div>
 </nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
 	import DemoVideo from '$lib/assets/videos/riju-demo-05-10-2023.mp4'
 	import MetaData from '$lib/components/MetaData.svelte'
 	import { Button } from '$lib/components/ui/button'
-	import * as Card from '$lib/components/ui/card'
 	import Footer from '$lib/ui/Footer.svelte'
 	import HomePageNavbar from '$lib/ui/HomePageNavbar.svelte'
 </script>
@@ -14,29 +13,124 @@
 
 <HomePageNavbar />
 
-<main class="mx-auto max-w-4xl p-4">
+<main class="mx-auto max-w-3xl">
 	<!-- hero -->
-	<section class="text-center max-w-4xl mx-auto">
-		<h1 class="text-5xl mb-4">Create presentation in seconds</h1>
-		<p class="mb-12 text-muted-foreground text-lg tracking-wide">
-			Just write content, Riju will convert it into beautiful presentation. Just
-			like magic!
+	<section class="text-center">
+		<h1 class="text-5xl mb-6">Hyper Fast Presentation Builder.</h1>
+		<p class="px-16 text-muted-foreground text-xl leading-normal mb-8">
+			Riju is a hyper fast presentation builder that allows you to create
+			stunning presentations in seconds. No more boring Drag-&-Drop, choose from
+			100+ stunning themes and much more. Its an easy-to-use alternative to
+			Microsoft PowerPoint.
 		</p>
 
-		<div class="flex justify-center gap-3">
-			<Button size="lg" href="/login" data-sveltekit-preload-data
-				>Create Account (Its Free)</Button
+		<div class="flex justify-center gap-3 mb-2">
+			<Button size="lg" class="text-lg py-6" href="/login">
+				Get started (its free)</Button
+			>
+			<Button
+				variant="secondary"
+				size="lg"
+				class="text-lg py-6"
+				href="/live-demo">Live demo</Button
 			>
 		</div>
+		<p class="text-muted-foreground">
+			No credit card required. Saves 90% of your time.
+		</p>
 	</section>
 
 	<!-- demo -->
 	<section>
-		<Card.Root>
-			<Card.Header>
-				<video class="w-full rounded" src={DemoVideo} muted loop autoplay />
-			</Card.Header>
-		</Card.Root>
+		<video
+			class="w-full rounded-lg shadow-lg"
+			src={DemoVideo}
+			muted
+			loop
+			autoplay
+		/>
+	</section>
+
+	<!-- letter -->
+	<section
+		class="prose lg:prose-lg prose-p:text-lg prose-p:text-muted-foreground prose-p:leading-relaxed mx-auto"
+	>
+		<h2>It's time to ditch Microsoft PowerPoint</h2>
+
+		<p>
+			Microsoft PowerPoint, often time-consuming and overkill for most users,
+			can be a real productivity drain with its excessive features and limited
+			themes, often overwhelming users and making the simplest of tasks
+			unnecessarily complicated.
+		</p>
+
+		<p>
+			Hours of hackathon experience drove the creation of Riju, a product born
+			from my need for efficient presentation-building. Winning six of thirteen
+			hackathons highlighted the pivotal role of impactful presentations for
+			innovative ideas.
+		</p>
+
+		<p>
+			<strong>Riju</strong> is more than just a presentation builder; it's a game-changer.
+			Here are some of its key features:
+		</p>
+
+		<h3>Rapid Presentation Creation</h3>
+		<p>
+			Riju allows users to create presentations in a matter of seconds. Simply
+			write your content, and Riju will automatically convert it into a
+			well-structured and visually appealing presentation.
+		</p>
+
+		<h3>Effortless Theme Customization</h3>
+		<p>
+			Changing presentation themes has never been easier. With Riju, you can
+			switch themes with a single click, ensuring that your presentation aligns
+			perfectly with your message and audience.
+		</p>
+
+		<h3>Hyper Speed</h3>
+		<p>
+			Riju is designed for efficiency. You'll be amazed at how quickly you can
+			produce high-quality presentations, saving you valuable time.
+		</p>
+
+		<h3>User-Friendly Interface</h3>
+		<p>
+			Our intuitive interface ensures that anyone, regardless of their technical
+			skills, can create captivating presentations effortlessly.
+		</p>
+
+		<h3>Cost-Effective Alternative</h3>
+		<p>
+			Riju provides a cost-effective and user-friendly alternative to
+			traditional presentation software like Microsoft PowerPoint, making it
+			accessible to individuals and businesses alike.
+		</p>
+
+		<p>
+			P.S: As an indie-hacker, I am passionate about delivering a product that
+			simplifies the presentation creation process, helping individuals and
+			teams make their ideas shine. Riju is not just a tool; it's a solution to
+			the challenges that arise when presenting ideas in a competitive
+			landscape.
+		</p>
+
+		<p>I hope you enjoy Riju.</p>
+
+		<p>Sincerely,</p>
+
+		<p><strong>Vignesh Aithal</strong></p>
+	</section>
+
+	<!-- cta -->
+	<section class="max-w-lg mx-auto text-center">
+		<h2 class="text-destructive">Ready to ditch PowerPoint?</h2>
+		<h2 class="mb-6">Try it now!</h2>
+		<Button size="lg" class="text-lg py-6" href="/login">
+			Get started (its free)</Button
+		>
 	</section>
 
 	<Footer />
@@ -44,6 +138,6 @@
 
 <style lang="postcss">
 	section {
-		@apply my-40;
+		@apply my-32;
 	}
 </style>
