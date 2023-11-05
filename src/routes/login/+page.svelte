8794c625f3398d424d4cf8d63a3af76c6cee1b4e
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-11-05 14:09:28 +0530

    chore(login): fine tune login page fix #76

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -16,47 +16,6 @@
 <HomePageNavbar />
 
 <section class="max-w-md mx-auto mt-8">
-	<Card.Root>
-		<Card.Header>
-			<Card.Title>Sign In</Card.Title>
-			<Card.Description
-				>You can login and signup from this same page.</Card.Description
-			>
-		</Card.Header>
-		<Card.Content>
-			<Input
-				type="email"
-				placeholder="Email"
-				disabled={isEmailSent}
-				bind:value={email}
-			/>
-			<p class="text-sm text-muted-foreground">
-				By clicking below, you agree to the Riju <a
-					class="underline"
-					href="/docs/terms">Terms of Service</a
-				>
-				and <a class="underline" href="/docs/privacy">Privacy Policy</a>.
-			</p>
-		</Card.Content>
-		<Card.Footer class="grid grid-cols-1 gap-4">
-			<Button
-				disabled={isEmailSent}
-				on:click={async () => {
-					isEmailSent = true
-					isEmailSent = await signInWithMagicLink(email)
-				}}
-				>{isEmailSent ? 'Loading...' : 'Continue'}
-			</Button>
-			<p class="text-center">OR</p>
-			<Button
-				variant="destructive"
-				on:click={async () => {
-					signInWithGooogle()
-				}}>Sign in with Google</Button
-			>
-		</Card.Footer>
-	</Card.Root>
-
 	{#if isEmailSent}
 		<Alert.Root class="mt-6">
 			<Alert.Title>Success!</Alert.Title>
@@ -65,5 +24,46 @@
 				to acccess dashboard.
 			</Alert.Description>
 		</Alert.Root>
+	{:else}
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>Sign In</Card.Title>
+				<Card.Description
+					>You can login and signup from this same page.</Card.Description
+				>
+			</Card.Header>
+			<Card.Content>
+				<Input
+					type="email"
+					placeholder="Email"
+					disabled={isEmailSent}
+					bind:value={email}
+				/>
+				<p class="text-sm text-muted-foreground">
+					By clicking below, you agree to the Riju <a
+						class="underline"
+						href="/docs/terms">Terms of Service</a
+					>
+					and <a class="underline" href="/docs/privacy">Privacy Policy</a>.
+				</p>
+			</Card.Content>
+			<Card.Footer class="grid grid-cols-1 gap-4">
+				<Button
+					disabled={isEmailSent}
+					on:click={async () => {
+						isEmailSent = true
+						isEmailSent = await signInWithMagicLink(email)
+					}}
+					>{isEmailSent ? 'Loading...' : 'Continue'}
+				</Button>
+				<p class="text-center">OR</p>
+				<Button
+					variant="destructive"
+					on:click={async () => {
+						signInWithGooogle()
+					}}>Sign in with Google</Button
+				>
+			</Card.Footer>
+		</Card.Root>
 	{/if}
 </section>
