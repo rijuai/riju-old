fc8582fea70b3825616dff559b70ac3662c1558d
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-11-06 20:50:00 +0530

    Add sticky navbar with background blur effect

--- src/lib/ui/HomePageNavbar.svelte
+++ src/lib/ui/HomePageNavbar.svelte
@@ -2,7 +2,9 @@
 	import { Button } from '$lib/components/ui/button'
 </script>
 
-<nav class="flex justify-between py-3.5 px-2 max-w-6xl mx-auto">
+<nav
+	class="sticky top-0 z-10 w-full flex justify-between py-2.5 px-2 max-w-6xl mx-auto bg-background/60 backdrop-blur"
+>
 	<Button variant="link" class="text-3xl font-semibold" href="/">Riju</Button>
 	<div class="flex gap-2.5 items-center">
 		<Button variant="link" href="/docs/contact">Contact</Button>
