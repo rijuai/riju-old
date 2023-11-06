70a266c49d35a0d7c3a2b05022ca0fd43c83e99f
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-11-06 22:44:24 +0530

    Add UI label and update styles for success alert and sign in card.

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -4,6 +4,7 @@
 	import Button from '$lib/components/ui/button/button.svelte'
 	import * as Card from '$lib/components/ui/card'
 	import Input from '$lib/components/ui/input/input.svelte'
+	import { Label } from '$lib/components/ui/label'
 	import { signInWithGooogle, signInWithMagicLink } from '$lib/db/auth'
 	import HomePageNavbar from '$lib/ui/HomePageNavbar.svelte'
 
@@ -18,7 +19,7 @@
 <section class="max-w-md mx-auto mt-8">
 	{#if isEmailSent}
 		<Alert.Root class="mt-6">
-			<Alert.Title>Success!</Alert.Title>
+			<Alert.Title class="text-xl">Success!</Alert.Title>
 			<Alert.Description>
 				An email has been sent for verification. Please click on that email link
 				to acccess dashboard.
@@ -26,42 +27,57 @@
 		</Alert.Root>
 	{:else}
 		<Card.Root>
-			<Card.Header>
-				<Card.Title>Sign In</Card.Title>
-				<Card.Description
-					>You can login and signup from this same page.</Card.Description
-				>
+			<Card.Header class="space-y-1">
+				<Card.Title class="text-2xl">Create an account</Card.Title>
+				<Card.Description>
+					You can also sign in from this same page.
+				</Card.Description>
 			</Card.Header>
-			<Card.Content>
-				<Input
-					type="email"
-					placeholder="Email"
-					disabled={isEmailSent}
-					bind:value={email}
-				/>
+			<Card.Content class="grid gap-4">
+				<div class="flex flex-col gap-6">
+					<Button
+						variant="destructive"
+						on:click={async () => {
+							signInWithGooogle()
+						}}>Sign in with Google</Button
+					>
+				</div>
+				<div class="relative">
+					<div class="absolute inset-0 flex items-center">
+						<span class="w-full border-t" />
+					</div>
+					<div class="relative flex justify-center text-xs uppercase">
+						<span class="bg-card px-2 text-muted-foreground">
+							Or continue with
+						</span>
+					</div>
+				</div>
+				<div class="grid gap-2">
+					<Label for="email">Email</Label>
+					<Input
+						id="email"
+						type="email"
+						placeholder="me@example.com"
+						disabled={isEmailSent}
+						bind:value={email}
+					/>
+				</div>
 				<p class="text-sm text-muted-foreground">
-					By clicking below, you agree to the Riju <a
+					By clicking, you agree to the Riju <a
 						class="underline"
 						href="/docs/terms">Terms of Service</a
 					>
 					and <a class="underline" href="/docs/privacy">Privacy Policy</a>.
 				</p>
 			</Card.Content>
-			<Card.Footer class="grid grid-cols-1 gap-4">
+			<Card.Footer>
 				<Button
+					class="w-full"
 					disabled={isEmailSent}
 					on:click={async () => {
 						isEmailSent = true
 						isEmailSent = await signInWithMagicLink(email)
-					}}
-					>{isEmailSent ? 'Loading...' : 'Continue'}
-				</Button>
-				<p class="text-center">OR</p>
-				<Button
-					variant="destructive"
-					on:click={async () => {
-						signInWithGooogle()
-					}}>Sign in with Google</Button
+					}}>{isEmailSent ? 'Loading...' : 'Create account'}</Button
 				>
 			</Card.Footer>
 		</Card.Root>
