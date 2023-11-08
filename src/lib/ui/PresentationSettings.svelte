105d179e1b0f74d349d2e9899200607d1d2ec274
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-11-08 17:08:16 +0530

    Add presentation settings and navbar options

---
+++ src/lib/ui/PresentationSettings.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button'
+	import * as Dialog from '$lib/components/ui/dialog'
+	import { Label } from '$lib/components/ui/label'
+	import * as Select from '$lib/components/ui/select'
+	import { transitionType } from '$lib/stores/presenter'
+	import { MoreVertical } from 'lucide-svelte'
+</script>
+
+<Dialog.Root>
+	<Dialog.Trigger
+		><Button variant="ghost" size="icon">
+			<MoreVertical />
+		</Button></Dialog.Trigger
+	>
+	<Dialog.Content class="max-w-sm">
+		<Dialog.Header>
+			<Dialog.Title>Settings</Dialog.Title>
+		</Dialog.Header>
+		<div class="grid gap-4 py-4">
+			<div class="grid grid-cols-2 items-center gap-4">
+				<Label>Tranisition Type</Label>
+				<Select.Root
+					selected={$transitionType}
+					onSelectedChange={(value) => {
+						$transitionType = value?.value
+					}}
+				>
+					<Select.Trigger>
+						<Select.Value placeholder="Select" />
+					</Select.Trigger>
+					<Select.Content>
+						<Select.Item value="none">None</Select.Item>
+						<Select.Item value="slide">Slide</Select.Item>
+						<Select.Item value="fade">Fade</Select.Item>
+						<Select.Item value="zoom">Zoom</Select.Item>
+						<Select.Item value="concave">Concave</Select.Item>
+						<Select.Item value="convex">Convex</Select.Item>
+					</Select.Content>
+				</Select.Root>
+			</div>
+		</div>
+	</Dialog.Content>
+</Dialog.Root>
--- src/lib/ui/PresenterNavbar.svelte
+++ src/lib/ui/PresenterNavbar.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
 	import { goto } from '$app/navigation'
+	import FeedbackDialog from '$lib/components/FeedbackDialog.svelte'
 	import { Button } from '$lib/components/ui/button'
-	import * as Dialog from '$lib/components/ui/dialog'
-	import { Label } from '$lib/components/ui/label'
-	import * as Select from '$lib/components/ui/select'
-	import { changeTheme, transitionType } from '$lib/stores/presenter'
-	import { Expand, MoreVertical, Sparkles, X } from 'lucide-svelte'
+	import { changeTheme } from '$lib/stores/presenter'
+	import { Expand, Sparkles, X } from 'lucide-svelte'
+	import PresentationSettings from './PresentationSettings.svelte'
 
 	export let presentationId: string
 
@@ -21,40 +20,12 @@
 </script>
 
 <nav
-	class="fixed w-full z-50 flex items-center justify-between px-2.5 py-2 bg-transparent bg-opacity-90 backdrop-blur-lg"
+	class="fixed top-0 w-full z-10 flex place-items-cente items-center justify-between px-2.5 py-2 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
 >
 	<Button variant="link" class="text-2xl" href="/">Riju</Button>
-	<div class="flex gap-1.5">
-		<Dialog.Root>
-			<Dialog.Trigger type="button"><MoreVertical /></Dialog.Trigger>
-			<Dialog.Content>
-				<Dialog.Header>
-					<Dialog.Title>Theme Settings</Dialog.Title>
-				</Dialog.Header>
-				<div class="grid gap-4 py-4">
-					<div class="grid grid-cols-4 items-center gap-4">
-						<Label>Tranisition</Label>
-						<Select.Root
-							selected={$transitionType}
-							onSelectedChange={(value) => {
-								$transitionType = value?.value ?? 'slide'
-							}}
-						>
-							<Select.Trigger class="">
-								<Select.Value placeholder="Slide" />
-							</Select.Trigger>
-							<Select.Content>
-								<Select.Item value="none">None</Select.Item>
-								<Select.Item value="slide">Slide</Select.Item>
-								<Select.Item value="zoom">Zoom</Select.Item>
-								<Select.Item value="concave">Concave</Select.Item>
-								<Select.Item value="convex">Convex</Select.Item>
-							</Select.Content>
-						</Select.Root>
-					</div>
-				</div>
-			</Dialog.Content>
-		</Dialog.Root>
+	<div class="flex gap-2">
+		<FeedbackDialog />
+		<PresentationSettings />
 		<Button variant="ghost" on:click={changeTheme}
 			><Sparkles class="mr-2" />Change Theme</Button
 		>
