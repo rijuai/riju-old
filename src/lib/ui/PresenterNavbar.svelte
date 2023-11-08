72cded70927eb6a4700fb4eadff2f103fcbed95a
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-11-08 18:46:33 +0530

    Add popover component and theme settings popup

--- src/lib/ui/PresenterNavbar.svelte
+++ src/lib/ui/PresenterNavbar.svelte
@@ -2,9 +2,8 @@
 	import { goto } from '$app/navigation'
 	import FeedbackDialog from '$lib/components/FeedbackDialog.svelte'
 	import { Button } from '$lib/components/ui/button'
-	import { changeTheme } from '$lib/stores/presenter'
-	import { Expand, Sparkles, X } from 'lucide-svelte'
-	import PresentationSettings from './PresentationSettings.svelte'
+	import { Expand, X } from 'lucide-svelte'
+	import ThemeSettingsPopup from './ThemeSettingsPopup.svelte'
 
 	export let presentationId: string
 
@@ -23,12 +22,9 @@
 	class="fixed top-0 w-full z-10 flex place-items-cente items-center justify-between px-2.5 py-2 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
 >
 	<Button variant="link" class="text-2xl" href="/">Riju</Button>
-	<div class="flex gap-2">
+	<div class="flex gap-3">
 		<FeedbackDialog />
-		<PresentationSettings />
-		<Button variant="ghost" on:click={changeTheme}
-			><Sparkles class="mr-2" />Change Theme</Button
-		>
+		<ThemeSettingsPopup />
 		<Button
 			variant="ghost"
 			size="icon"
---
+++ src/lib/ui/ThemeSettingsPopup.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte'
+	import Label from '$lib/components/ui/label/label.svelte'
+	import * as Popover from '$lib/components/ui/popover'
+	import * as Select from '$lib/components/ui/select'
+	import { changeTheme, transitionType } from '$lib/stores/presenter'
+	import { Sparkles } from 'lucide-svelte'
+</script>
+
+<Popover.Root>
+	<Popover.Trigger class="flex items-center gap-1.5"
+		><Sparkles />Theme</Popover.Trigger
+	>
+	<Popover.Content>
+		<div class="flex flex-col gap-2">
+			<div class="grid grid-cols-2 items-center gap-3">
+				<Label>Theme</Label>
+				<Button
+					variant="outline"
+					size="sm"
+					class="text-xs"
+					on:click={() => {
+						changeTheme()
+					}}>Generate Random</Button
+				>
+
+				<Label>Tranisition Type</Label>
+				<Select.Root
+					selected={$transitionType}
+					onSelectedChange={(value) => {
+						$transitionType = value?.value
+					}}
+				>
+					<Select.Trigger>
+						<Select.Value placeholder="Select" />
+					</Select.Trigger>
+					<Select.Content>
+						<Select.Item value="none">None</Select.Item>
+						<Select.Item value="slide">Slide</Select.Item>
+						<Select.Item value="fade">Fade</Select.Item>
+						<Select.Item value="zoom">Zoom</Select.Item>
+						<Select.Item value="concave">Concave</Select.Item>
+						<Select.Item value="convex">Convex</Select.Item>
+					</Select.Content>
+				</Select.Root>
+			</div>
+		</div>
+	</Popover.Content>
+</Popover.Root>
+
+<style lang="postcss">
+</style>
