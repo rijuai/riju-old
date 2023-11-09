74d51bd0922814710f2393121d5d004a7a2fa33a
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-11-09 12:21:00 +0530

    Refactor presentation list display logic in dashboard page.

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -9,20 +9,20 @@
 {#await getPresentations()}
 	<Loader class="mx-auto animate-spin mt-8" />
 {:then presentations}
-	{#if presentations !== undefined && presentations.length > 0}
+	{#if presentations !== undefined}
 		<div class="grid grid-cols-5 gap-8">
 			{#each presentations as { presentation_id, title }}
 				<a href="/dashboard/editor?id={presentation_id}">
 					<div class="w-full h-32 bg-zinc-200 rounded mb-2.5" />
 					<p class="truncate text-[14px]">{title}</p>
 				</a>
+			{:else}
+				<p class="max-w-md mx-auto text-center text-sm">
+					No presentations found. Click on the "Create" button to create a new
+					presentation.
+				</p>
 			{/each}
 		</div>
-	{:else}
-		<p class="max-w-md mx-auto text-center text-sm">
-			No presentations found. Click on the "Create" button to create a new
-			presentation.
-		</p>
 	{/if}
 {:catch error}
 	<p>Error: {error.message}</p>
