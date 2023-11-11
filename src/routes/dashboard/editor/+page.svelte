baf2ff10aeb41784484aed4f94ac6c5477870d36
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-11-11 11:23:24 +0530

    Add presentation ID to page editor.

--- src/routes/dashboard/editor/+page.svelte
+++ src/routes/dashboard/editor/+page.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
+	import { page } from '$app/stores'
 	import Editor from '$lib/components/Editor.svelte'
 	import HelpDialog from '$lib/components/HelpDialog.svelte'
 	import MetaData from '$lib/components/MetaData.svelte'
+	import { presentationId } from '$lib/stores/presenter'
+	import { onMount } from 'svelte'
+
+	onMount(() => {
+		$presentationId = getPresentationId()
+	})
+
+	const getPresentationId = () => {
+		return $page.url.searchParams.get('id') ?? ''
+	}
 </script>
 
 <MetaData
