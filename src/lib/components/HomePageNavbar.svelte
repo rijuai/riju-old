4e208096db287717472a6485881fdd4ab1d532b4
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-11-11 12:22:39 +0530

    Refactor HomePageNavbar component layout

--- src/lib/components/HomePageNavbar.svelte
+++ src/lib/components/HomePageNavbar.svelte
@@ -3,12 +3,14 @@
 </script>
 
 <nav
-	class="sticky top-0 z-10 w-full flex justify-between py-2.5 px-2 max-w-6xl mx-auto bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
+	class="sticky top-0 z-10 w-full py-2.5 px-2 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
 >
-	<Button variant="link" class="text-3xl font-semibold" href="/">Riju</Button>
-	<div class="flex gap-2.5 items-center">
-		<Button variant="ghost" href="/docs/contact">Contact</Button>
-		<Button variant="ghost" href="/login">Sign In</Button>
-		<Button href="/login">Get started</Button>
+	<div class="max-w-6xl mx-auto flex justify-between">
+		<Button variant="link" class="text-3xl font-semibold" href="/">Riju</Button>
+		<div class="flex gap-2.5 items-center">
+			<Button variant="ghost" href="/docs/contact">Contact</Button>
+			<Button variant="ghost" href="/login">Sign In</Button>
+			<Button href="/login">Get started</Button>
+		</div>
 	</div>
 </nav>
