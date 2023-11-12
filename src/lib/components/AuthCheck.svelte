4ea6b5da0880a0ad75ca74f56b23c8bc87f02b61
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-11-12 19:24:08 +0530

    Refactor AuthCheck component and remove unused store

--- src/lib/components/AuthCheck.svelte
+++ src/lib/components/AuthCheck.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
 	import { goto } from '$app/navigation'
 	import { supabase } from '$lib/config/supabase'
-	import { isUserSignedIn } from '$lib/stores/user'
 	import { onMount } from 'svelte'
+
+	let isUserAuthenticated = false
 
 	onMount(async () => {
 		supabase.auth.onAuthStateChange(async (event, session) => {
 			if (!session) goto('/signin')
-			if (session && session.user) $isUserSignedIn = true
+			if (session && session.user) isUserAuthenticated = true
 		})
 	})
 </script>
 
-{#if $isUserSignedIn}
+{#if isUserAuthenticated}
 	<slot />
 {/if}
