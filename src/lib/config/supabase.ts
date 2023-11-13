24a4d4ce0fd6a7daa4df1a2d4cadbfd53cfa67ee
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-11-13 17:15:42 +0530

    Import SUPABASE_ANON_KEY and SUPABASE_URL from environment variables.

--- src/lib/config/supabase.ts
+++ src/lib/config/supabase.ts
@@ -1,7 +1,4 @@
+import { SUPABASE_ANON_KEY, SUPABASE_URL } from '$env/static/private'
 import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://xrqtyfvyqmwhdnuyskip.supabase.co'
-const SUPABASE_ANON_KEY =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhycXR5ZnZ5cW13aGRudXlza2lwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI1MTIzMDUsImV4cCI6MjAwODA4ODMwNX0.fS48YgFiPMHbyunud5vQO3lAe3maHOAlJyn-_-3ZuWg'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
