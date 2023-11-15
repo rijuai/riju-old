62b662f6b229db14266ef042bf42476ef19cef91
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-11-15 13:08:19 +0530

    Add transition type select menu to ThemeSettingsPopup component

--- src/lib/components/ThemeSettingsPopup.svelte
+++ src/lib/components/ThemeSettingsPopup.svelte
@@ -2,8 +2,36 @@
 	import Button from '$lib/components/ui/button/button.svelte'
 	import Label from '$lib/components/ui/label/label.svelte'
 	import * as Popover from '$lib/components/ui/popover'
-	import { changeTheme } from '$lib/stores/presentation'
+	import * as Select from '$lib/components/ui/select'
+	import { changeTheme, transitionType } from '$lib/stores/presentation'
 	import { Sparkles } from 'lucide-svelte'
+
+	let transitionTypes = [
+		{
+			label: 'None',
+			value: 'none',
+		},
+		{
+			label: 'Slide',
+			value: 'slide',
+		},
+		{
+			label: 'Fade',
+			value: 'fade',
+		},
+		{
+			label: 'Zoom',
+			value: 'zoom',
+		},
+		{
+			label: 'Concave',
+			value: 'concave',
+		},
+		{
+			label: 'Convex',
+			value: 'convex',
+		},
+	]
 </script>
 
 <Popover.Root>
@@ -24,7 +52,7 @@
 				>
 			</div>
 
-			<!-- <div class="grid grid-cols-2 items-center gap-3">
+			<div class="grid grid-cols-2 items-center gap-3">
 				<Label>Tranisition Type</Label>
 				<Select.Root
 					selected={$transitionType}
@@ -36,15 +64,12 @@
 						<Select.Value placeholder="Select" />
 					</Select.Trigger>
 					<Select.Content>
-						<Select.Item value="none">None</Select.Item>
-						<Select.Item value="slide">Slide</Select.Item>
-						<Select.Item value="fade">Fade</Select.Item>
-						<Select.Item value="zoom">Zoom</Select.Item>
-						<Select.Item value="concave">Concave</Select.Item>
-						<Select.Item value="convex">Convex</Select.Item>
+						{#each transitionTypes as { label, value }}
+							<Select.Item {value}>{label}</Select.Item>
+						{/each}
 					</Select.Content>
 				</Select.Root>
-			</div> -->
+			</div>
 
 			<!-- use this for new rows -->
 			<!-- <div class="grid grid-cols-2 items-center gap-3" /> -->
