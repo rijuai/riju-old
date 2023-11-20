378ad6ba6d4902bbdc29a473bfb456901e4dab1c
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-11-20 17:04:02 +0530

    Update social media links in Footer component

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -4,11 +4,14 @@
 
 <footer class="flex flex-col justify-center gap-6 py-8">
 	<div class="flex justify-center gap-6">
-		<a href="https://twitter.com/RijuPage"><Twitter /></a>
-		<a href="https://www.youtube.com/channel/UCqCC3zl-WFdSujoAyKANCUg"
-			><Youtube />
-		</a>
-		<a href="https://www.linkedin.com/company/rijuai"><Linkedin /></a>
+		<a href="https://twitter.com/RijuPage" aria-label="Twitter"><Twitter /></a>
+		<a
+			href="https://www.youtube.com/channel/UCqCC3zl-WFdSujoAyKANCUg"
+			aria-label="YouTube"><Youtube /></a
+		>
+		<a href="https://www.linkedin.com/company/rijuai" aria-label="LinkedIn"
+			><Linkedin /></a
+		>
 	</div>
 	<div class="flex justify-center gap-6 text-xs text-muted-foreground">
 		<a href="/docs/contact">Contact</a>
