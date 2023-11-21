d010936cbdffaaa53a3824e24c3e785a0ea00b0e
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-11-21 11:10:10 +0530

    Refactor table component imports

--- src/lib/components/ui/table/table-footer.svelte
+++ src/lib/components/ui/table/table-footer.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-	import { cn } from "$lib/utils";
-	import type { HTMLAttributes } from "svelte/elements";
+	import { cn } from '$lib/utils'
+	import type { HTMLAttributes } from 'svelte/elements'
 
-	type $$Props = HTMLAttributes<HTMLTableSectionElement>;
+	type $$Props = HTMLAttributes<HTMLTableSectionElement>
 
-	let className: $$Props["class"] = undefined;
-	export { className as class };
+	let className: $$Props['class'] = undefined
+	export { className as class }
 </script>
 
 <tfoot
-	class={cn("bg-primary font-medium text-primary-foreground", className)}
+	class={cn('bg-primary font-medium text-primary-foreground', className)}
 	{...$$restProps}
 >
 	<slot />
