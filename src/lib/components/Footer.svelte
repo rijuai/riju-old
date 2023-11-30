86ad8525aabaadfc67b97070efd4254345d52f94
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-11-30 22:27:15 +0530

    Update Footer component styling

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,19 +1,5 @@
-<script lang="ts">
-	import { Linkedin, Twitter, Youtube } from 'lucide-svelte'
-</script>
-
-<footer class="flex flex-col justify-center gap-6 py-8">
-	<div class="flex justify-center gap-6">
-		<a href="https://twitter.com/RijuPage" aria-label="Twitter"><Twitter /></a>
-		<a
-			href="https://www.youtube.com/channel/UCqCC3zl-WFdSujoAyKANCUg"
-			aria-label="YouTube"><Youtube /></a
-		>
-		<a href="https://www.linkedin.com/company/rijuai" aria-label="LinkedIn"
-			><Linkedin /></a
-		>
-	</div>
-	<div class="flex justify-center gap-6 text-xs text-muted-foreground">
+<footer class="flex justify-center py-4 bg-slate-80">
+	<div class="flex justify-center gap-6 text-sm text-muted-foreground">
 		<a href="/docs/contact">Contact</a>
 		<a href="/docs/privacy">Privacy Policy</a>
 		<a href="/docs/terms">Terms</a>
