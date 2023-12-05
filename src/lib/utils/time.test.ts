c1a6142fb418eeccaa236998627c2b4c5fa5788b
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-12-05 12:05:47 +0530

    Remove getCurrentTime test from time.test.ts

--- src/lib/utils/time.test.ts
+++
@@ -1,7 +0,0 @@
-import dayjs from 'dayjs'
-import { getCurrentTime } from './time'
-
-test('Should return current time', () => {
-	const time = dayjs().format()
-	expect(getCurrentTime()).toBe(time)
-})
