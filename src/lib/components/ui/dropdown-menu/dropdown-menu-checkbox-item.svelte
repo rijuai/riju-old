5ddaf5662a81172f37bc15d9be1abf5c8f630f04
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-12-07 13:20:34 +0530

    Refactor dropdown menu imports

--- src/lib/components/ui/dropdown-menu/dropdown-menu-checkbox-item.svelte
+++ src/lib/components/ui/dropdown-menu/dropdown-menu-checkbox-item.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { DropdownMenu as DropdownMenuPrimitive } from 'bits-ui'
 	import { cn } from '$lib/utils'
+	import { DropdownMenu as DropdownMenuPrimitive } from 'bits-ui'
 	import { Check } from 'lucide-svelte'
 
 	type $$Props = DropdownMenuPrimitive.CheckboxItemProps
