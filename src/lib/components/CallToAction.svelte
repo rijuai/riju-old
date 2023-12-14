2a65a586715c7fc7538fef86d7e2fad8c0275fcf
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-12-14 11:44:44 +0530

    Add ChevronRight icon to Get Started button

--- src/lib/components/CallToAction.svelte
+++ src/lib/components/CallToAction.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { Button } from '$lib/components/ui/button'
+	import { ChevronRight } from 'lucide-svelte'
 </script>
 
 <section class="max-w-l mx-auto text-center py-24 border-t">
@@ -8,10 +9,10 @@
 		<!-- <h3 class="mb-8">Then switch to Riju</h3> -->
 		<h4 class="mb-4">Then use Riju to build presentations in seconds.</h4>
 		<div class="flex flex-col md:flex-row justify-center gap-4 mb-4">
-			<Button size="lg" class="text-lg py-6" href="/signin">Get Started</Button>
+			<Button size="lg" class="text-lg py-6" href="/signin"
+				>Get Started<ChevronRight class="ml-2" /></Button
+			>
 		</div>
-		<p class="text-muted-foreground">
-			Saves 90% of your time. No credit card required.
-		</p>
+		<p class="text-muted-foreground">Saves 90% of your time</p>
 	</div>
 </section>
