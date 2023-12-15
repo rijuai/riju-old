2d462f1e8dadf4fac1d43a26e238a82b6bfbca98
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-12-15 17:06:33 +0530

    Reorder import statements in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-import { type ClassValue, clsx } from 'clsx'
+import { clsx, type ClassValue } from 'clsx'
 import { cubicOut } from 'svelte/easing'
 import type { TransitionConfig } from 'svelte/transition'
 import { twMerge } from 'tailwind-merge'
