96905f363d47804b0fd8de98f64b1222d65cde6d
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-12-21 17:38:18 +0530

    Add error page fix #129

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+	import { Button } from '$lib/components/ui/button'
+</script>
+
+<main class="h-screen flex flex-col justify-center items-center">
+	<h1 class="mb-4">{$page.status}: {$page.error?.message}</h1>
+	<p class="mb-8 text-muted-foreground">
+		This page doesn't exist. If you think there is something off, please contact
+		us.
+	</p>
+	<Button href="/">Go back home</Button>
+</main>
