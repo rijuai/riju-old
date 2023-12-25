db0ccaeb301ab71929f22efd7b8eaffbbc14eae8
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-12-25 11:35:37 +0530

    Update CallToAction component layout and content

--- src/lib/components/CallToAction.svelte
+++ src/lib/components/CallToAction.svelte
@@ -3,16 +3,13 @@
 	import { ChevronRight } from 'lucide-svelte'
 </script>
 
-<section class="max-w-l mx-auto text-center py-24 border-t">
-	<div class="max-w-lg mx-auto">
-		<h1 class="text-destructive mb-2">Hate Building Presentations?</h1>
-		<!-- <h3 class="mb-8">Then switch to Riju</h3> -->
-		<h4 class="mb-4">Then use Riju to build presentations in seconds.</h4>
-		<div class="flex flex-col md:flex-row justify-center gap-4 mb-4">
+<section class="max-w-l mx-auto text-center py-32">
+	<div class="max-w-2xl mx-auto">
+		<h1 class="text-5xl font-medium mb-16">Presentations reimagined!</h1>
+		<div class="flex flex-col md:flex-row justify-center gap-4 mb-2">
 			<Button size="lg" class="text-lg py-6" href="/signin"
 				>Get Started<ChevronRight class="ml-2" /></Button
 			>
 		</div>
-		<p class="text-muted-foreground">Saves 90% of your time</p>
 	</div>
 </section>
