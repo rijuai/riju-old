b19cb76554e753c2081668f23659512cac7f7eac
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-12-26 12:40:01 +0530

    Update Footer component styles

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,10 +1,10 @@
 <footer
-	class="max-w-6xl mx-auto flex justify-between py-4 px-4 bg-slate-80 border-t"
+	class="max-w-6xl mx-auto flex flex-col sm:flex-row gap-4 justify-center sm:justify-between py-4 px-4 bg-slate-80 border-t"
 >
-	<div class="text-sm text-muted-foreground">
+	<div class="text-sm text-muted-foreground text-center">
 		Riju is built with love from India.
 	</div>
-	<div class="flex justify-center gap-8 text-sm text-muted-foreground">
+	<div class="flex justify-center gap-8 text-xs text-muted-foreground">
 		<a href="/docs/contact">Contact</a>
 		<a href="/docs/privacy">Privacy Policy</a>
 		<a href="/docs/terms">Terms</a>
