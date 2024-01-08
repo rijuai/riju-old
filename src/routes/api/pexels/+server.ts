fe687ba29dd9a525815ba9e485911720ac799703
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2024-01-08 15:39:54 +0530

    Add Access-Control-Allow-Origin header to API response

--- src/routes/api/pexels/+server.ts
+++ src/routes/api/pexels/+server.ts
@@ -7,5 +7,11 @@ export const GET: RequestHandler = async ({ url }) => {
 
 	const photos = await pexels.photos.search({ query, per_page: 5 })
 
-	return json({ photos: photos })
+	return json({
+		photos: photos,
+	}, {
+		headers: {
+			'Access-Control-Allow-Origin': 'https://riju.ai/*'
+		}
+	})
 }
