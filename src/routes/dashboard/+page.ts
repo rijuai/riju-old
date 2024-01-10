89eba029d549f468cdf6c6540389f75fa76c66a6
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2024-01-10 21:26:05 +0530

    Don't prerender /dashboard

--- src/routes/dashboard/+page.ts
+++ src/routes/dashboard/+page.ts
@@ -1,11 +1,9 @@
-export const prerender = true
-
 import { getPresentations } from '$lib/db/presentation'
 import type { PageLoad } from './$types'
 
 export const load = (async () => {
 	const presentations = await getPresentations()
-	
+
 	return {
 		presentations,
 	}
