ea14933720d7025a5de0f265293b0c60ef21e21f
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2024-01-12 11:00:40 +0530

    Update HomePageNavbar component

--- src/lib/components/HomePageNavbar.svelte
+++ src/lib/components/HomePageNavbar.svelte
@@ -3,23 +3,26 @@
 </script>
 
 <nav
-	class="sticky top-0 z-50 w-full p-2 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/70 border-b"
+	class="sticky top-0 z-50 max-w-6xl mx-auto p-2 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/70"
 >
-	<div class="max-w-6xl mx-auto flex justify-between">
+	<div class="flex justify-between">
 		<Button variant="link" class="text-2xl font-semibold" href="/">Riju</Button>
-		<div class="flex gap-2.5 items-center">
+		<div class="flex gap-2 items-center">
 			<Button
 				variant="ghost"
 				href="https://giant-anise-fba.notion.site/Riju-Change-log-327736186a0c4a4b92621a104569917f"
-				class="hidden md:block">Change Log</Button
+				class="hidden md:block">Change log</Button
+			>
+			<Button variant="ghost" href="/docs/pricing" class="hidden md:block"
+				>Pricing</Button
 			>
 			<Button variant="ghost" href="/docs/contact" class="hidden md:block"
 				>Contact</Button
 			>
 			<Button variant="ghost" href="/signin" class="hidden md:block"
-				>Sign In</Button
+				>Log in</Button
 			>
-			<Button href="/signin" class="gap-2">Get Started</Button>
+			<Button href="/signin" class="gap-2">Sign up</Button>
 		</div>
 	</div>
 </nav>
