0ed6064ca2100e465ed7ea6b313d453c6dff8827
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2024-01-14 11:50:31 +0530

    Update Editor component styles

--- src/lib/components/Editor.svelte
+++ src/lib/components/Editor.svelte
@@ -129,7 +129,7 @@
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <div
 	id="editor"
-	class="p-2 min-h-screen mb-8 max-w-3xl mx-auto prose prose-sm prose-h1:font-medium prose-h2:font-medium prose-h1:text-3xl prose-h2:text-2xl prose-h3:text-xl prose-p:text-lg"
+	class="p-8 min-h-screen mb-8 max-w-4xl mx-auto prose prose-sm prose-h1:font-medium prose-h2:font-medium prose-h1:text-3xl prose-h2:text-2xl prose-h3:text-xl prose-p:text-lg border rounded-lg shadow-md"
 	class:hidden={showLoader}
 	bind:this={element}
 	on:click={() => {
