dbba6e38082c90b9578c028542883e501beddf7d
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2024-01-14 12:03:48 +0530

    Update HomePageNavbar styling

--- src/lib/components/HomePageNavbar.svelte
+++ src/lib/components/HomePageNavbar.svelte
@@ -3,10 +3,10 @@
 </script>
 
 <nav
-	class="sticky top-0 z-50 p-2 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/70"
+	class="sticky top-0 z-50 px-2 py-3 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/70"
 >
 	<div class="flex justify-between">
-		<Button variant="link" class="text-2xl font-semibold" href="/">Riju</Button>
+		<Button variant="link" class="text-3xl font-semibold" href="/">Riju</Button>
 		<div class="flex gap-2 items-center">
 			<!-- <Button
 				variant="ghost"
