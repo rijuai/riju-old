f150153403a4ce2d944af2834e8770e42c5c120f
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2024-01-14 12:55:07 +0530

    Update navbar styling

--- src/lib/components/DashboardNavbar.svelte
+++ src/lib/components/DashboardNavbar.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <nav
-	class="sticky p-2 top-0 z-50 max-w-7xl mx-auto flex gap-1 justify-between items-center  bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
+	class="sticky px-2 py-3 top-0 z-50 max-w-7xl mx-auto flex gap-1 justify-between items-center bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
 >
 	<Button variant="link" class="text-2xl" href="/dashboard"
 		>Riju
