66abb5c85ece5e1ae37cb83a60ce56f7a501240b
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2024-01-22 20:25:57 +0530

    Refactor getPresentationContent function to use Supabase

--- src/routes/dashboard/editor/[presentation_id]/+page.ts
+++ src/routes/dashboard/editor/[presentation_id]/+page.ts
@@ -1,5 +1,17 @@
-import { getPresentationContent } from '$lib/db/presentation'
+import { supabase } from '$lib/config/supabase'
+import type { Json } from '../../../../schema'
 import type { PageLoad } from './$types'
+
+const getPresentationContent = async (
+	presentationId: string,
+): Promise<{ content: Json } | null> => {
+	const { data } = await supabase
+		.from('presentations')
+		.select('content')
+		.eq('id', presentationId)
+
+	return data ? data[0] : null
+}
 
 export const load = (async ({ params }) => {
 	const presentationId = params.presentation_id
