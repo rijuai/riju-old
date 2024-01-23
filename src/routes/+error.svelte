a7adbd634b8cecb89579ac857812432e86076a4b
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2024-01-23 20:59:53 +0530

    Update error page layout

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,7 +3,9 @@
 	import { Button } from '$lib/components/ui/button'
 </script>
 
-<main class="h-screen flex flex-col justify-center items-center">
+<main
+	class="h-screen flex flex-col justify-center items-center text-center p-4"
+>
 	<h1 class="mb-4">{$page.status}: {$page.error?.message}</h1>
 	<p class="mb-8 text-muted-foreground">
 		This page doesn't exist. If you think there is something off, please contact
