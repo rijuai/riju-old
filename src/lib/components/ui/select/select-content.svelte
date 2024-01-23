a273e9a67493442c4ee1c06b8c67d64a9107eb9a
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2024-01-23 21:45:16 +0530

    Migrate to Shadcn Svelte Newyork theme

--- src/lib/components/ui/select/select-content.svelte
+++ src/lib/components/ui/select/select-content.svelte
@@ -4,8 +4,8 @@
 	import { scale } from 'svelte/transition'
 
 	type $$Props = SelectPrimitive.ContentProps
-	type $$Events = SelectPrimitive.ContentEvents
 
+	let className: $$Props['class'] = undefined
 	export let sideOffset: $$Props['sideOffset'] = 4
 	export let inTransition: $$Props['inTransition'] = flyAndScale
 	export let inTransitionConfig: $$Props['inTransitionConfig'] = undefined
@@ -15,8 +15,6 @@
 		opacity: 0,
 		duration: 50,
 	}
-
-	let className: $$Props['class'] = undefined
 	export { className as class }
 </script>
 
@@ -27,11 +25,10 @@
 	{outTransitionConfig}
 	{sideOffset}
 	class={cn(
-		'relative z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md outline-none',
+		'relative z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md focus:outline-none',
 		className,
 	)}
 	{...$$restProps}
-	on:keydown
 >
 	<div class="w-full p-1">
 		<slot />
