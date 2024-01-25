5372db9ed55c93f6c216c66da8041433b89be5ae
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2024-01-25 11:21:56 +0530

    Refactor CTA component

---
+++ src/lib/components/CTA.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { Button } from "$lib/components/ui/button";
+    import { ChevronRight } from "lucide-svelte";
+</script>
+
+<section class="max-w-l mx-auto px-4 py-32 text-center">
+    <div class="mx-auto max-w-2xl">
+        <h2 class="mb-4">Presentations reimagined!</h2>
+        <p class="text-muted-foreground mb-12 text-2xl">
+            Building presentations with Riju is as simple as one-two-three.
+        </p>
+        <div class="mb-3 flex flex-col justify-center gap-4 md:flex-row">
+            <Button size="lg" class="py-6 text-lg" href="/signin"
+                >Get Started<ChevronRight class="ml-2" /></Button
+            >
+        </div>
+        <p class="text-muted-foreground text-sm">No credit card required</p>
+    </div>
+</section>
