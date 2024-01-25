65ef3ecef04d24ea97d451da0aefec02b3cf3dd0
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2024-01-25 11:28:06 +0530

    Add types.d.ts file with Item, ListItem, Content, Marks, Presentations, and TransitionType definitions

---
+++ src/lib/types.d.ts
@@ -0,0 +1,45 @@
+type Item = {
+  [x: string]: undefined;
+  type: string;
+  content: Content[];
+  attrs: {
+    start: number;
+    src: string;
+    alt: string;
+    level: number;
+  };
+  text: string;
+};
+
+type ListItem = {
+  type: string;
+  content: Content;
+};
+
+type Content = {
+  type: string;
+  marks: Marks[];
+  text: string;
+  content: {
+    text: string;
+    marks: Marks[];
+  }[];
+};
+
+type Marks = {
+  type: string;
+};
+
+type Presentations = {
+  id: string;
+  title: string;
+}[];
+
+enum TransitionType {
+  None = "none",
+  Slide = "slide",
+  Fade = "fade",
+  Zoom = "zoom",
+  Concave = "concave",
+  Convex = "convex",
+}
