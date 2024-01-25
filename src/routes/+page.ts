222d80d146877c043bb00878ef687241b906aebd
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2024-01-25 22:51:07 +0530

    Fix import formatting and add authentication check

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,9 +1,9 @@
-import { isUserAuthenticated } from '$lib/db/auth'
-import { redirect } from '@sveltejs/kit'
-import type { PageLoad } from './$types'
+import { isUserAuthenticated } from "$lib/db/auth";
+import { redirect } from "@sveltejs/kit";
+import type { PageLoad } from "./$types";
 
-export const prerender = true
+export const prerender = true;
 
 export const load: PageLoad = async () => {
-	// if (await isUserAuthenticated()) redirect(302, '/dashboard')
-}
+  if (await isUserAuthenticated()) redirect(302, "/dashboard");
+};
