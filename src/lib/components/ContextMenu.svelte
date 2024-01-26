59ae7cd89ade6c68396f250aabb985c923119cda
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2024-01-26 19:23:40 +0530

    Refactor ContextMenu.svelte component

--- src/lib/components/ContextMenu.svelte
+++ src/lib/components/ContextMenu.svelte
@@ -10,78 +10,56 @@
         UnderlineIcon,
     } from "lucide-svelte";
     import { onDestroy } from "svelte";
-    import Button from "./ui/button/button.svelte";
-    import Input from "./ui/input/input.svelte";
 
     export let editor: Editor;
 
-    let showAiSearchBar = false;
-
     onDestroy(() => {
         console.log("Context Menu Destroyed");
-        showAiSearchBar = false;
     });
 </script>
 
-{#if showAiSearchBar}
-    <Toggle
-        size="sm"
-        aria-label="toggle strike"
-        pressed={editor.isActive("strike")}
-        class="font-normal text-fuchsia-600"
-        on:click={() => {
-            editor.chain().focus().toggleStrike().run();
-        }}
-        ><Sparkles class="mr-2" />Ask AI
-    </Toggle>
-    <Separator orientation="vertical" />
-    <Toggle
-        size="sm"
-        aria-label="toggle bold"
-        pressed={editor.isActive("bold")}
-        on:click={() => {
-            editor.chain().focus().toggleBold().run();
-        }}
-    >
-        <Bold />
-    </Toggle>
-    <Toggle
-        size="sm"
-        aria-label="toggle italic"
-        pressed={editor.isActive("italic")}
-        on:click={() => {
-            editor.chain().focus().toggleItalic().run();
-        }}
-    >
-        <Italic />
-    </Toggle>
-    <Toggle
-        size="sm"
-        aria-label="toggle underline"
-        pressed={editor.isActive("underline")}
-        on:click={() => {
-            editor.chain().focus().toggleUnderline().run();
-        }}
-        ><UnderlineIcon />
-    </Toggle>
-    <Toggle
-        size="sm"
-        aria-label="toggle strike"
-        pressed={editor.isActive("strike")}
-        on:click={() => {
-            editor.chain().focus().toggleStrike().run();
-        }}
-        ><Strikethrough />
-    </Toggle>
-{/if}
-
-<Input
-    class="hover::ring-0 w-full rounded-r-none"
-    placeholder="Search for images"
-    on:keydown={(e) => {
-        if (e.key === "Enter") {
-            showAiSearchBar = true;
-        }
+<Separator orientation="vertical" />
+<Toggle
+    size="sm"
+    aria-label="toggle bold"
+    pressed={editor.isActive("bold")}
+    on:click={() => {
+        editor.chain().focus().toggleBold().run();
     }}
-/>
-<Button variant="secondary" class="rounded-l-none">Search</Button>
+>
+    <Bold />
+</Toggle>
+<Toggle
+    size="sm"
+    aria-label="toggle italic"
+    pressed={editor.isActive("italic")}
+    on:click={() => {
+        editor.chain().focus().toggleItalic().run();
+    }}
+>
+    <Italic />
+</Toggle>
+<Toggle
+    size="sm"
+    aria-label="toggle underline"
+    pressed={editor.isActive("underline")}
+    on:click={() => {
+        editor.chain().focus().toggleUnderline().run();
+    }}
+    ><UnderlineIcon />
+</Toggle>
+<Toggle
+    size="sm"
+    aria-label="toggle strike"
+    pressed={editor.isActive("strike")}
+    on:click={() => {
+        editor.chain().focus().toggleStrike().run();
+    }}
+    ><Strikethrough />
+</Toggle>
+
+<style lang="postcss">
+    :global(svg) {
+        @apply h-4 w-4;
+    }
+</style>
