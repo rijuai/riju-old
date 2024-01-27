1829ced32407b66d6ed46f9aad2435eda8862796
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2024-01-27 23:29:27 +0530

    Refactor code in dashboard page module

--- src/routes/dashboard/+page.ts
+++ src/routes/dashboard/+page.ts
@@ -1,21 +1,19 @@
-import { supabase } from '$lib/config/supabase'
-import type { PageLoad } from './$types'
+import { supabase } from "$lib/config/supabase";
+import type { PageLoad } from "./$types";
 
-const getPresentations = async (): Promise<
-	{ id: string; title: string | null }[] | null
-> => {
-	const { data } = await supabase
-		.from('presentations')
-		.select('id, title')
-		.order('updated_at', { ascending: false })
+const getPresentations = async () => {
+  const { data } = await supabase
+    .from("presentations")
+    .select("id, title")
+    .order("updated_at", { ascending: false });
 
-	return data ? data : null
-}
+  return data ? data : null;
+};
 
 export const load = (async () => {
-	const presentations = await getPresentations()
+  const presentations = await getPresentations();
 
-	return {
-		presentations,
-	}
-}) satisfies PageLoad
+  return {
+    presentations,
+  };
+}) satisfies PageLoad;
