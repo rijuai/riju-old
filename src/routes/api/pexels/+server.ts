c8a150090871a0b82b229a25fd0359530b635cc4
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2024-01-29 22:25:38 +0530

    Add API_ALLOWED_ORIGIN import and update GET request handler in pexels server

--- src/routes/api/pexels/+server.ts
+++ src/routes/api/pexels/+server.ts
@@ -1,17 +1,15 @@
+import { API_ALLOWED_ORIGIN } from "$env/static/private";
 import { pexels } from "$lib/server/pexels";
 import { json } from "@sveltejs/kit";
 import type { RequestHandler } from "@sveltejs/kit";
-import { API_ALLOWED_ORIGIN } from "$env/static/private";
 
-export const GET: RequestHandler = async ({ request }) => {
+export const GET: RequestHandler = async ({ request, url }) => {
   const origin = request.headers.get("host");
   if (!origin || origin !== API_ALLOWED_ORIGIN) {
     return new Response("Sorry, Forbidden", { status: 403 });
   }
 
-  const url = new URL(request.url);
   const query = url.searchParams.get("query") ?? "";
-
   const photos = await pexels.photos.search({ query, per_page: 5 });
 
   return json({
