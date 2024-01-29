7ec36841f44b75417bdab70fff0fbfe43ba58f69
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2024-01-29 18:17:32 +0530

    Update dashboard layout and add Navbar component

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
     import AuthCheck from "./AuthCheck.svelte";
-    import DashboardNavbar from "./DashboardNavbar.svelte";
+    import Navbar from "./Navbar.svelte";
 </script>
 
 <AuthCheck>
-    <DashboardNavbar />
+    <Navbar />
 
     <main class="px-6 py-4">
         <slot />
