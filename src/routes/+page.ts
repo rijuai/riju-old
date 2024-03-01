cd5cfa0518d4abe779b98717edcefd6393123be4
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2024-03-01 13:34:24 +0530

    Update +page.ts

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,10 +1,12 @@
 // import { isUserAuthenticated } from "$lib/db/auth";
 import { redirect } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
+import pb from "$lib/pocketbase";
 
 export const ssr = false;
 export const prerender = true;
 
 export const load: PageLoad = async () => {
-  // if (await isUserAuthenticated()) redirect(302, "/dashboard");
+  let isUserAuthenticated = pb.authStore.isValid;
+  if (isUserAuthenticated) redirect(302, "/dashboard");
 };
