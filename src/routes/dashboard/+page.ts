d230610d1ecbb028c0824a7d7c0134a7cee21ee5
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2024-03-15 19:49:04 +0530

    Format code

--- src/routes/dashboard/+page.ts
+++ src/routes/dashboard/+page.ts
@@ -4,7 +4,7 @@ import pb from "$lib/pocketbase";
 export const load: PageLoad = async () => {
   const presentations = await pb.collection("presentations").getFullList({
     sort: "-updated",
-    fields: "id,title",
+    fields: "id, title",
   });
 
   return {
