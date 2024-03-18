7eac37be2798009d394743fbaeaa02bf1b0e497d
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2024-03-18 14:57:11 +0530

    Fix: AuthCheck not redirecting after logout

--- src/routes/dashboard/AuthCheck.svelte
+++ src/routes/dashboard/AuthCheck.svelte
@@ -1,24 +1,19 @@
 <script lang="ts">
     import { goto } from "$app/navigation";
     import pb from "$lib/pocketbase";
-    import { onDestroy, onMount } from "svelte";
+    import { onDestroy } from "svelte";
     import { userId } from "$lib/stores/user";
 
     let isUserAuthenticated = false;
 
     const removeListener = pb.authStore.onChange((token, model) => {
-        console.log("New store data 1:", token, model);
-        $userId = model?.id;
-    });
-
-    onMount(async () => {
         if (pb.authStore.isValid) {
             $userId = pb.authStore.model?.id;
             isUserAuthenticated = true;
         } else {
-            goto("/signin");
+            goto("/");
         }
-    });
+    }, true);
 
     onDestroy(() => {
         removeListener();
