aee3c93e7250eb180fe8b40bdfbaf7cb2f69defa
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2024-08-29 14:41:03 +0530

    chore: Format code according to JavaScript formatting rules

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,6 +1,6 @@
-import { PUBLIC_POCKETBASE_URL } from "$env/static/public";
-import PocketBase from "pocketbase";
+import { PUBLIC_POCKETBASE_URL } from "$env/static/public"
+import PocketBase from "pocketbase"
 
-const pb = new PocketBase(PUBLIC_POCKETBASE_URL);
+const pb = new PocketBase(PUBLIC_POCKETBASE_URL)
 
-export default pb;
+export default pb
