3355f20d36bf3786dec66521021d46a6294ba57d
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2024-08-29 14:41:22 +0530

    chore: Update JavaScript formatting rules

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from "@sveltejs/adapter-cloudflare";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "@sveltejs/adapter-cloudflare"
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,6 +11,6 @@ const config = {
 			$lib: "./src/lib",
 		},
 	},
-};
+}
 
-export default config;
+export default config
