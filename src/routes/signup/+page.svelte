5846a337aaab14ed9ee0b5b589342921ee12bce5
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2025-02-18 16:08:46 +0530

    refactor: Remove docs routes and related components

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -41,12 +41,5 @@ const signUpUsingEmailAndPassword = async () => {
 		<Input type="email" placeholder="Email" bind:value={email} />
 		<Input type="password" placeholder="Password" bind:value={password} />
 		<Button class="w-full" on:click={signUpUsingEmailAndPassword}>Continue</Button>
-		<!-- <p class="text-muted-foreground mb-12 text-xs">
-			By clicking, you agree to Riju <a class="underline" href="/docs/terms">Terms of Service</a>
-			and <a class="underline" href="/docs/privacy">Privacy Policy</a>.
-		</p>
-		<p class="text-center text-sm">
-			Already have an account <a class="underline" href="/login">Login</a>
-		</p> -->
 	</div>
 </section>
