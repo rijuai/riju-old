10f428d51bf77acbb22d22b286619589319740e4
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2025-03-04 11:40:22 +0530

    fix: Disable prerendering for main page

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -3,7 +3,6 @@ import { redirect } from '@sveltejs/kit'
 import type { PageLoad } from './$types'
 
 export const ssr = false
-export const prerender = true
 
 export const load: PageLoad = async () => {
 	const isUserAuthenticated = pb.authStore.isValid
