4c64cb3d8915e4c55e5809b9438d9d57f80da60e
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2025-03-24 22:52:51 +0530

    fix: update PostMetadata interface and add new blog post
    - Reformatted the PostMetadata interface for consistency in code style.
    - Added a new Markdown file for the blog post titled "Creating the Best Presentation," including metadata and key points for readers.
    - This enhances the blog section with fresh content and maintains code clarity.

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,15 +1,15 @@
 import type { SvelteComponent } from 'svelte'
 
 export interface PostMetadata {
-    title: string
-    description?: string
-    date?: string
-    author?: string
-    image?: string
-    layout: string
+	title: string
+	description?: string
+	date?: string
+	author?: string
+	image?: string
+	layout: string
 }
 
 export interface Post {
-    metadata: PostMetadata
-    default: typeof SvelteComponent
-} 
+	metadata: PostMetadata
+	default: typeof SvelteComponent
+}
