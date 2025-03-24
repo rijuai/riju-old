ef1214e5e78a07d23f15a7a2970548bb8f345666
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2025-03-24 14:45:39 +0530

    style: adjust CTA component layout and text
    - Reduced padding in the CTA section for a more compact design.
    - Updated heading to remove unnecessary margin for improved visual consistency.
    - Enhanced button size for better accessibility and user experience.

--- src/lib/components/CTA.svelte
+++ src/lib/components/CTA.svelte
@@ -3,17 +3,17 @@ import { Button } from '$lib/components/ui/button'
 import ChevronRight from 'lucide-svelte/icons/chevron-right'
 </script>
 
-<section class="px-4 py-32 text-center">
+<section class="px-4 py-24 text-center">
 	<div class="mx-auto max-w-2xl">
-		<h2 class="mb-4">Presentations reimagined!</h2>
+		<h2>Presentations reimagined!</h2>
 		<p class="text-muted-foreground mb-16 text-xl">
 			Building presentations with Riju is as simple as one-two-three.
 		</p>
 		<div class="space-y-4">
-			<Button href="/signup"
+			<Button href="/signup" size="lg"
 				>Get Started<ChevronRight class="ml-2 size-4" /></Button
 			>
-			<p class="text-muted-foreground text-sm">
+			<p class="text-muted-foreground">
 				No credit card required. Fully free.
 			</p>
 		</div>
