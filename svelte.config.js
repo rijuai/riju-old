6e32cc2f69436bca6c3d658000dab545803eefe0
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2025-03-25 09:32:42 +0530

    Revert "refactor: update dropdown menu components for improved props handling"

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,16 @@
-import { mdsvex } from "mdsvex";
-import adapter from "@sveltejs/adapter-node";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "@sveltejs/adapter-node"
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [vitePreprocess(), mdsvex({ layout: "./src/routes/blog/BlogLayout.svelte" })],
+	preprocess: vitePreprocess(),
+
 	kit: {
 		adapter: adapter(),
-		alias: { $lib: "./src/lib" }
+		alias: {
+			$lib: "./src/lib",
+		},
 	},
-	extensions: [".svelte", ".svx", "md"]
-};
+}
 
-export default config;
+export default config
