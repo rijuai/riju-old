0114896f3acda848371dc47c16096fbc062283a4
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2025-03-25 10:40:21 +0530

    fix: update Svelte config for improved layout and extensions
    - Refactored the Svelte config to enhance the preprocess array formatting for better readability.
    - Updated the layout path for mdsvex to './src/routes/blog/BlogLayout.svelte' and removed the 'md' extension from the extensions array.
    - This change streamlines the configuration and maintains consistency in the project structure.

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,17 @@
-import { mdsvex } from "mdsvex";
-import adapter from "@sveltejs/adapter-node";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { mdsvex } from 'mdsvex'
+import adapter from '@sveltejs/adapter-node'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 const config = {
-	preprocess: [vitePreprocess(), mdsvex({ layout: "./src/routes/blog/BlogLayout.svelte" })],
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({ layout: './src/routes/blog/BlogLayout.svelte' })
+	],
 	kit: {
 		adapter: adapter(),
-		alias: { $lib: "./src/lib" }
+		alias: { $lib: './src/lib' }
 	},
-	extensions: [".svelte", ".svx", "md"]
-};
+	extensions: ['.svelte', '.svx']
+}
 
-export default config;
+export default config
