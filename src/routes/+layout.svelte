b3dfeef7bbaf8bdc38de260aea01d63afa85f1cb
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2025-04-03 12:12:40 +0530

    Add Alpha feedback banner to the layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
-import '../app.css'
-import { initAuth } from '$lib/stores/auth'
-import { onMount } from 'svelte'
+    import "../app.css";
+    import { initAuth } from "$lib/stores/auth";
+    import { onMount } from "svelte";
 
-let { children } = $props()
+    let { children } = $props();
 
-onMount(() => {
-	initAuth()
-})
+    onMount(() => {
+        initAuth();
+    });
 </script>
 
+<div class="bg-yellow-300 text-slate-800 font-semibold p-3 text-center text-sm">
+    Currently Riju is in Alpha. Give your feedback -> <a
+        class="underline"
+        href="https://tally.so/r/wAvyoN">click here</a
+    > (We're always listening).
+</div>
 {@render children()}
